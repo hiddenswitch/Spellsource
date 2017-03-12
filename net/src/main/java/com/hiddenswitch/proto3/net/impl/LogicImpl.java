862ba40a20db18b5b5913dab701c4235b4e878e5
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-03-12 16:54:56 -0400

    Automatically add test decks

--- net/src/main/java/com/hiddenswitch/proto3/net/impl/LogicImpl.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/impl/LogicImpl.java
@@ -2,6 +2,7 @@ package com.hiddenswitch.proto3.net.impl;
 
 import co.paralleluniverse.fibers.SuspendExecution;
 import co.paralleluniverse.fibers.Suspendable;
+import com.hiddenswitch.proto3.net.Decks;
 import com.hiddenswitch.proto3.net.Inventory;
 import com.hiddenswitch.proto3.net.Logic;
 import com.hiddenswitch.proto3.net.Service;
@@ -11,8 +12,10 @@ import com.hiddenswitch.proto3.net.util.Broker;
 import com.hiddenswitch.proto3.net.util.ServiceProxy;
 import io.vertx.ext.mongo.MongoClientUpdateResult;
 import net.demilich.metastone.game.Attribute;
+import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.cards.desc.CardDesc;
 import net.demilich.metastone.game.decks.Deck;
+import net.demilich.metastone.game.decks.DeckCatalogue;
 import net.demilich.metastone.game.events.BeforeSummonEvent;
 import net.demilich.metastone.game.events.GameEventType;
 import net.demilich.metastone.game.spells.desc.trigger.EventTriggerArg;
@@ -22,10 +25,9 @@ import net.demilich.metastone.game.targeting.EntityReference;
 import net.demilich.metastone.game.utils.AttributeMap;
 import org.apache.commons.lang3.RandomStringUtils;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.hiddenswitch.proto3.net.util.QuickJson.json;
@@ -36,22 +38,53 @@ import static io.vertx.ext.sync.Sync.awaitResult;
  */
 public class LogicImpl extends Service<LogicImpl> implements Logic {
 	private ServiceProxy<Inventory> inventory;
+	private ServiceProxy<Decks> decks;
 
 	@Override
 	public void start() throws SuspendExecution {
 		super.start();
 		Broker.of(this, Logic.class, vertx.eventBus());
 		inventory = Broker.proxy(Inventory.class, vertx.eventBus());
+		decks = Broker.proxy(Decks.class, vertx.eventBus());
 	}
 
 	@Override
 	@Suspendable
 	public InitializeUserResponse initializeUser(InitializeUserRequest request) throws SuspendExecution, InterruptedException {
 		final InitializeUserResponse response = new InitializeUserResponse();
+		response.setDeckCreateResponses(new ArrayList<>());
 		final String userId = request.getUserId();
 		final CreateCollectionRequest startingCollection = CreateCollectionRequest.startingCollection(userId);
 		response.setCreateCollectionResponse(inventory.sync()
 				.createCollection(startingCollection));
+
+		// Create the starting decks
+		try {
+			DeckCatalogue.loadDecksFromPackage();
+		} catch (IOException | URISyntaxException e) {
+			throw new RuntimeException();
+		}
+
+		if (DeckCatalogue.getDecks().size() > 0) {
+			for (String deckName : new String[]{"Basic Resurrector", "Basic Chef"}) {
+				Deck deck = DeckCatalogue.getDeckByName(deckName);
+				if (deck == null) {
+					continue;
+				}
+				// Give the player the cards they need for their starting decks
+				final AddToCollectionResponse addToCollectionResponse = inventory.sync().addToCollection(new AddToCollectionRequest()
+						.withUserId(userId)
+						.withCardIds(deck.getCards().toList().stream().map(Card::getCardId).collect(Collectors.toList())));
+				final DeckCreateResponse deckCreate = decks.sync().createDeck(new DeckCreateRequest()
+						.withUserId(userId)
+						.withHeroClass(deck.getHeroClass())
+						.withName(deck.getName())
+						.withInventoryIds(addToCollectionResponse.getInventoryIds()));
+				response.getDeckCreateResponses().add(deckCreate);
+				response.getCreateCollectionResponse().getCreatedInventoryIds().addAll(addToCollectionResponse.getInventoryIds());
+			}
+		}
+
 
 		return response;
 	}
--- net/src/main/java/com/hiddenswitch/proto3/net/models/InitializeUserResponse.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/models/InitializeUserResponse.java
@@ -1,12 +1,14 @@
 package com.hiddenswitch.proto3.net.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by bberman on 1/30/17.
  */
 public class InitializeUserResponse implements Serializable {
 	private CreateCollectionResponse createCollectionResponse;
+	private List<DeckCreateResponse> deckCreateResponses;
 
 	public CreateCollectionResponse getCreateCollectionResponse() {
 		return createCollectionResponse;
@@ -18,6 +20,19 @@ public class InitializeUserResponse implements Serializable {
 
 	public InitializeUserResponse withCreateCollectionResponse(final CreateCollectionResponse createCollectionResponse) {
 		this.createCollectionResponse = createCollectionResponse;
+		return this;
+	}
+
+	public List<DeckCreateResponse> getDeckCreateResponses() {
+		return deckCreateResponses;
+	}
+
+	public void setDeckCreateResponses(List<DeckCreateResponse> deckCreateResponses) {
+		this.deckCreateResponses = deckCreateResponses;
+	}
+
+	public InitializeUserResponse withDeckCreateResponses(final List<DeckCreateResponse> deckCreateResponses) {
+		this.deckCreateResponses = deckCreateResponses;
 		return this;
 	}
 }
