0f10f1b98fb2cdfb9c010af411bc0099035f9b3f
Finaris <josephmatthewtorres@gmail.com>
2017-04-05 22:14:03 -0400

    Fixed decks

--- net/src/main/java/com/hiddenswitch/proto3/net/impl/LogicImpl.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/impl/LogicImpl.java
@@ -70,7 +70,7 @@ public class LogicImpl extends Service<LogicImpl> implements Logic {
 
 		if (DeckCatalogue.getDecks().size() > 0) {
 			for (String deckName : new String[]{"Basic Resurrector", "Basic Chef", "Basic CPU", "Basic Octopod",
-					"Basic Astronaut", "Basic Gamer", "Basic Biologist", "Basic Paleontologist", "Basic Octopod",}) {
+					"Basic Astronaut", "Basic Biologist", "Basic Paleontologist", "Basic Cyborg", "Basic Gamer"}) {
 				Deck deck = DeckCatalogue.getDeckByName(deckName);
 				if (deck == null) {
 					continue;
