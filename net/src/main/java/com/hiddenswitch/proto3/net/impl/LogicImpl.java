fafebfd22d9175afe9c89d107284d26533d9a34c
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-04-26 13:21:34 -0700

    Disable Basic Chef since it uses choose one cards

--- net/src/main/java/com/hiddenswitch/proto3/net/impl/LogicImpl.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/impl/LogicImpl.java
@@ -66,7 +66,7 @@ public class LogicImpl extends Service<LogicImpl> implements Logic {
 		}
 
 		if (DeckCatalogue.getDecks().size() > 0) {
-			for (String deckName : new String[]{"Basic Resurrector", "Basic Chef", "Basic CPU", "Basic Octopod",
+			for (String deckName : new String[]{"Basic Resurrector", /* "Basic Chef", */ "Basic CPU", "Basic Octopod",
 					"Basic Astronaut", "Basic Biologist", "Basic Paleontologist", "Basic Cyborg", "Basic Gamer"}) {
 				Deck deck = DeckCatalogue.getDeckByName(deckName);
 				if (deck == null) {
