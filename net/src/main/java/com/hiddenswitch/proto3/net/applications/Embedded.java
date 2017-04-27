fac995ffcea0e1706da73e8bfda44bb3c8e7f555
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-04-27 10:44:56 -0700

    Fix AI slowdown due to too much logging, change default casual deck to basic octopod demo, fix exception on trying to remove entities from zones, increased hover time for opposing minions zoom effect

--- net/src/main/java/com/hiddenswitch/proto3/net/applications/Embedded.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/applications/Embedded.java
@@ -1,7 +1,9 @@
 package com.hiddenswitch.proto3.net.applications;
 
+import ch.qos.logback.classic.Level;
 import com.hiddenswitch.proto3.net.impl.ServerImpl;
 import io.vertx.core.Vertx;
+import io.vertx.core.logging.LoggerFactory;
 
 /**
  * Created by bberman on 11/29/16.
@@ -9,6 +11,10 @@ import io.vertx.core.Vertx;
 public class Embedded {
 	public static void main(String args[]) {
 		System.setProperty("java.net.preferIPv4Stack", "true");
+		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory
+				.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+		root.setLevel(Level.ERROR);
+
 		Vertx vertx = Vertx.vertx();
 		vertx.deployVerticle(new ServerImpl());
 	}
