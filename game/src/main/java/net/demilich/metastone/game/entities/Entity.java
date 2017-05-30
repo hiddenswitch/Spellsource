a0c4b0ddf738ab3dc63e09f2ce6b32eaf8a6b072
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-05-29 23:12:02 -0400

    More documentation, finishing attribute

--- game/src/main/java/net/demilich/metastone/game/entities/Entity.java
+++ game/src/main/java/net/demilich/metastone/game/entities/Entity.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import net.demilich.metastone.game.Attribute;
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.logic.CustomCloneable;
 import net.demilich.metastone.game.logic.GameLogic;
 import net.demilich.metastone.game.targeting.EntityReference;
@@ -12,6 +13,27 @@ import net.demilich.metastone.game.targeting.IdFactory;
 import net.demilich.metastone.game.targeting.Zones;
 import net.demilich.metastone.game.utils.AttributeMap;
 
+/**
+ * An in-game entity.
+ * <p>
+ * Entities are targetable objects in a match. The player, hero, hero power card, minions, cards in hand,
+ * cards in deck, cards in graveyard, secrets and certain kinds of triggers are all entities.
+ * <p>
+ * Entities are only created, never destroyed. Entities have a {@link EntityLocation}; each location (index, zone and
+ * player) can have only one entity occupying it at any time. Destroyed entities go to the {@link Zones#GRAVEYARD} or
+ * {@link Zones#REMOVED_FROM_PLAY} zone. {@link EntityZone} lists located in the {@link Player} objects are reponsible
+ * for making sure entities are in only one place at a time.
+ * <p>
+ * Entities all have attributes, which contain their state. As simple maps, entity attributes can be manipulated,
+ * copied, etc. Most effects interact with an entity's attributes.
+ * <p>
+ * Entities are mutable. Use {@link #clone()} to create an "immutable" view of an entity. However, for effects that
+ * need copies of entities, typically a {@code getCopy()} method is used, like {@link Card#getCopy()}.
+ * <p>
+ * This entity class will contain all the game engine information. It is not suitable to show to clients directly,
+ * because it may contain information that should be secret from an opponent. For example, {@link net.demilich.metastone.game.spells.trigger.secrets.Secret}
+ * entities should have their description or card IDs visible to their opponents.
+ */
 public abstract class Entity extends CustomCloneable implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private static final int NO_OWNER = -1;
@@ -26,12 +48,30 @@ public abstract class Entity extends CustomCloneable implements Serializable {
 		super();
 	}
 
+	/**
+	 * Clone an entity, including its ID and location.
+	 * <p>
+	 * Use this method for emulating an "immutable" view on an entity. This kind of cloning is not suitable for most
+	 * gameplay situations, because using the clone will cause two entities with identical IDs and locations to exist.
+	 * Instead, a subclass will provide a {@code getCopy()} method that is more helpful for gameplay.
+	 *
+	 * @return An exact clone.
+	 */
 	@Override
 	public Entity clone() {
 		Entity clone = (Entity) super.clone();
 		return clone;
 	}
 
+	/**
+	 * Gets the specified attribute.
+	 * <p>
+	 * Attributes are {@link Integer}, {@link String}, {@link String[]} or {@link Enum} types.
+	 * @param attribute The attribute to look up.
+	 * @return The value of the attribute.
+	 * @see #getAttributeValue(Attribute) to get the value of {@link Integer} attributes.
+	 * @see Attribute for a list of attributes.
+	 */
 	public Object getAttribute(Attribute attribute) {
 		return getAttributes().get(attribute);
 	}
@@ -44,6 +84,12 @@ public abstract class Entity extends CustomCloneable implements Serializable {
 		return getAttributes().containsKey(attribute) ? (int) getAttributes().get(attribute) : 0;
 	}
 
+	/**
+	 * Gets the type of entity this is. These will very nearly match up with the classes, but are primarily used for
+	 * filters that e.g. draw a Spell or destroy all Secrets.
+	 *
+	 * @return An {@link EntityType}
+	 */
 	public abstract EntityType getEntityType();
 
 	public int getId() {
