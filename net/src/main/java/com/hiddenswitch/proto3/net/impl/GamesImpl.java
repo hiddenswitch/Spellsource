3b08621037a1957a64e3a1dbbba4e05c3f178fcc
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-05-31 22:49:05 -0400

    GamesImpl cleanup and documentation

--- net/src/main/java/com/hiddenswitch/proto3/net/impl/GamesImpl.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/impl/GamesImpl.java
@@ -26,6 +26,7 @@ import io.vertx.core.eventbus.ReplyException;
 import io.vertx.core.eventbus.ReplyFailure;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.http.ServerWebSocket;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.net.NetServer;
@@ -67,6 +68,15 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 		this.websocketPort = RandomUtils.nextInt(8081, 16200);
 	}
 
+	/**
+	 * Starts the Games service.
+	 * <p>
+	 * This implementation will load all the cards; create a legacy TCP socket service for Java clients; a Unity
+	 * WebSocket compatible service for Unity clients, and then broadcast itself on the cluster to be available for
+	 * use.
+	 *
+	 * @throws SuspendExecution
+	 */
 	@Override
 	@Suspendable
 	public void start() throws SuspendExecution {
@@ -95,7 +105,7 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 
 			server.connectHandler(socket -> {
 				socket.handler(fiberHandler(messageBuffer -> {
-					receiveNetSocketData(socket, messageBuffer);
+					handleLegacySocketMessage(socket, messageBuffer);
 				}));
 			});
 
@@ -122,58 +132,7 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 				}
 
 				socket.handler(fiberHandler(messageBuffer -> {
-					com.hiddenswitch.proto3.net.client.models.ClientToServerMessage message =
-							Configuration.getDefaultApiClient().getJSON().deserialize(messageBuffer.toString(),
-									com.hiddenswitch.proto3.net.client.models.ClientToServerMessage.class);
-
-					GameSession session = gameForSocket.getOrDefault(socket, null);
-
-					if (session != null) {
-						updateActivity(session);
-					}
-
-					switch (message.getMessageType()) {
-						case FIRST_MESSAGE:
-							// Make sure this connection is authorized
-							final String userId = message.getFirstMessage().getPlayerKey();
-							String secret = message.getFirstMessage().getPlayerSecret();
-							if (!keyToSecret.containsKey(userId) ||
-									!keyToSecret.get(userId).equals(secret)) {
-								throw new RuntimeException("Invalid secret.");
-							}
-
-							if (session == null) {
-								session = gameForUserId.get(userId);
-							}
-
-							updateActivity(session);
-
-							Client client = new WebSocketClient(socket, userId, session.getPlayer(userId).getId());
-							gameForSocket.put(client.getPrivateSocket(), session);
-
-							if (session.isGameReady()) {
-								// TODO: Remove references to the old socket
-								// Replace the client
-								session.onPlayerReconnected(session.getPlayer(userId), client);
-							} else {
-								session.onPlayerConnected(session.getPlayer(userId), client);
-							}
-							break;
-						case UPDATE_ACTION:
-							if (session == null) {
-								throw new RuntimeException();
-							}
-							final String messageId = message.getRepliesTo();
-							session.onActionReceived(messageId, message.getActionIndex());
-							break;
-						case UPDATE_MULLIGAN:
-							if (session == null) {
-								throw new RuntimeException();
-							}
-							final String messageId2 = message.getRepliesTo();
-							session.onMulliganReceived(messageId2, message.getDiscardedCardIndices());
-							break;
-					}
+					handleWebSocketMessage(socket, messageBuffer);
 				}));
 			});
 
@@ -187,6 +146,12 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 		logger.debug("GamesImpl::start Registered on event bus.");
 	}
 
+	/**
+	 * Gets the {@link ServerGameContext} that corresponds to the given game ID.
+	 *
+	 * @param gameId The game to look up.
+	 * @return The game context, or {@code null} if this {@link Games} instance doesn't have it.
+	 */
 	public ServerGameContext getGameContext(String gameId) {
 		GameSession session = this.getGames().get(gameId);
 		if (session == null) {
@@ -195,6 +160,12 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 		return session.getGameContext();
 	}
 
+	/**
+	 * Gets the {@link GameSession} that corresponds to the given game ID.
+	 *
+	 * @param gameId The game to look up.
+	 * @return The game session, or {@code null} if this {@link Games} instance doesn't have it.
+	 */
 	public GameSession getGameSession(String gameId) {
 		return this
 				.getGames()
@@ -236,8 +207,18 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 		return DescribeGameSessionResponse.fromGameContext(getGameContext(request.getGameId()));
 	}
 
+	/**
+	 * The legacy message handler. Handles TCP clients using the Java serialization-based TCP API.
+	 * <p>
+	 * Messages are of the format {8x magic bytes}, followed by a {4xbyte 32bit content length}, followed by the
+	 * content. {@link IncomingMessage} is responsible for concatenating the network data until a complete message is
+	 * received. Once it is received, the message is deserialized to a {@link ClientToServerMessage} and is processed.
+	 *
+	 * @param socket        The socket corresponding to the server-client connection.
+	 * @param messageBuffer The buffer containing data from the client. It may be part of a message.
+	 */
 	@Suspendable
-	private void receiveNetSocketData(NetSocket socket, Buffer messageBuffer) {
+	private void handleLegacySocketMessage(NetSocket socket, Buffer messageBuffer) {
 		logger.trace("Getting buffer from socket with hashCode {} length {}. Incoming message count: {}", socket.hashCode(), messageBuffer.length(), netMessages.size());
 		// Do we have a reader for this socket?
 		ClientToServerMessage message = null;
@@ -298,7 +279,7 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 			return;
 		}
 
-		updateActivity(session);
+		keepAlive(session);
 
 		switch (message.getMt()) {
 			case FIRST_MESSAGE:
@@ -326,11 +307,89 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 		}
 
 		if (remainder != null) {
-			receiveNetSocketData(socket, remainder);
+			handleLegacySocketMessage(socket, remainder);
 		}
 	}
 
-	private void updateActivity(GameSession session) {
+	/**
+	 * The Unity Websocket client message handler.
+	 * <p>
+	 * A complete message is received and processed here. Users can only send 3 kinds of messages: <ul> <li>{@link
+	 * com.hiddenswitch.proto3.net.client.models.MessageType#FIRST_MESSAGE}: The first message from the client which
+	 * authenticates it to the server. </li> <li>{@link com.hiddenswitch.proto3.net.client.models.MessageType#UPDATE_ACTION}:
+	 * The client has chosen an action and it is packed into this message.</li> <li>{@link
+	 * com.hiddenswitch.proto3.net.client.models.MessageType#UPDATE_MULLIGAN}: The client has chosen which cards to
+	 * mulligan.</li> </ul>
+	 * <p>
+	 * All messages are deserialized into {@link com.hiddenswitch.proto3.net.client.models.ClientToServerMessage}
+	 * messages as generated by the Swagger specification.
+	 *
+	 * @param socket        The {@link ServerWebSocket} that represents the client's connection.
+	 * @param messageBuffer The message received from the client.
+	 */
+	@Suspendable
+	private void handleWebSocketMessage(ServerWebSocket socket, Buffer messageBuffer) {
+		com.hiddenswitch.proto3.net.client.models.ClientToServerMessage message =
+				Configuration.getDefaultApiClient().getJSON().deserialize(messageBuffer.toString(),
+						com.hiddenswitch.proto3.net.client.models.ClientToServerMessage.class);
+
+		GameSession session = gameForSocket.getOrDefault(socket, null);
+
+		if (session != null) {
+			keepAlive(session);
+		}
+
+		switch (message.getMessageType()) {
+			case FIRST_MESSAGE:
+				// Make sure this connection is authorized
+				final String userId = message.getFirstMessage().getPlayerKey();
+				String secret = message.getFirstMessage().getPlayerSecret();
+				if (!keyToSecret.containsKey(userId) ||
+						!keyToSecret.get(userId).equals(secret)) {
+					throw new RuntimeException("Invalid secret.");
+				}
+
+				if (session == null) {
+					session = gameForUserId.get(userId);
+				}
+
+				keepAlive(session);
+
+				Client client = new WebSocketClient(socket, userId, session.getPlayer(userId).getId());
+				gameForSocket.put(client.getPrivateSocket(), session);
+
+				if (session.isGameReady()) {
+					// TODO: Remove references to the old socket
+					// Replace the client
+					session.onPlayerReconnected(session.getPlayer(userId), client);
+				} else {
+					session.onPlayerConnected(session.getPlayer(userId), client);
+				}
+				break;
+			case UPDATE_ACTION:
+				if (session == null) {
+					throw new RuntimeException();
+				}
+				final String messageId = message.getRepliesTo();
+				session.onActionReceived(messageId, message.getActionIndex());
+				break;
+			case UPDATE_MULLIGAN:
+				if (session == null) {
+					throw new RuntimeException();
+				}
+				final String messageId2 = message.getRepliesTo();
+				session.onMulliganReceived(messageId2, message.getDiscardedCardIndices());
+				break;
+		}
+	}
+
+	/**
+	 * Updates an {@link ActivityMonitor} that a message was received from a client, and thus the {@link GameSession} is
+	 * still alive.
+	 *
+	 * @param session The session that has activity.
+	 */
+	private void keepAlive(GameSession session) {
 		String gameId = session.getGameId();
 		ActivityMonitor activityMonitor = gameActivityMonitors.get(gameId);
 		if (activityMonitor == null) {
@@ -373,7 +432,14 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 		return new ConcedeGameSessionResponse();
 	}
 
-	public void kill(String gameId) throws SuspendExecution, InterruptedException {
+	/**
+	 * Kills the specified game by ID.
+	 *
+	 * @param gameId The ID of the game to abruptly end.
+	 * @throws SuspendExecution
+	 * @throws InterruptedException
+	 */
+	private void kill(String gameId) throws SuspendExecution, InterruptedException {
 		GameSession session = games.get(gameId);
 
 		if (session == null) {
@@ -406,26 +472,52 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 		});
 
 		// Expire the match
-		expireMatch(gameId);
+		removeFromMatchmaker(gameId);
 
 		// Remove the game session
 		games.remove(gameId);
 	}
 
+	/**
+	 * Gets the port that the legacy game services are hosted at. Typically randomly generated.
+	 *
+	 * @return The port number.
+	 */
 	private int getLegacyPort() {
 		return legacyPort;
 	}
 
+	/**
+	 * The host that this server is running on.
+	 * <p>
+	 * Originally, this function would return the publicly-accessible DNS name of this host. But the hostname lookup
+	 * is broken, mysteriously, on Oracle Java 8 in Vert.x, and it hangs.
+	 *
+	 * @return {@code "0.0.0.0"} because the hosts are not aware of their publicly-accessible host names yet.
+	 */
 	private String getHost() {
 		// TODO: Look up this host correctly
 		return "0.0.0.0";
 	}
 
+	/**
+	 * Gets a map of the games by game ID.
+	 *
+	 * @return The game sessions map.
+	 */
 	private Map<String, GameSession> getGames() {
 		return Collections.unmodifiableMap(games);
 	}
 
-	private void expireMatch(String gameId) throws InterruptedException, SuspendExecution {
+	/**
+	 * Notifies the matchmaker that the game with the specified ID has expired and that, consequently, the player can
+	 * start a new game.
+	 *
+	 * @param gameId The game ID of the game to expire in the matchmaker.
+	 * @throws InterruptedException
+	 * @throws SuspendExecution
+	 */
+	private void removeFromMatchmaker(String gameId) throws InterruptedException, SuspendExecution {
 		try {
 			matchmaking.sync().expireOrEndMatch(new MatchExpireRequest(gameId));
 		} catch (VertxException vertxException) {
@@ -440,13 +532,23 @@ public class GamesImpl extends AbstractService<GamesImpl> implements Games {
 		}
 	}
 
+	/**
+	 * When a game session ends, this handler sets a timer to kill the game.
+	 *
+	 * @param session The session whose game ended naturally.
+	 */
 	@Suspendable
-	private void onGameOver(GameSessionImpl sgs) {
-		final String gameOverId = sgs.getGameId();
+	private void onGameOver(GameSession session) {
+		final String gameOverId = session.getGameId();
 		vertx.setTimer(CLEANUP_DELAY_MILLISECONDS, suspendableHandler(t -> kill(gameOverId)));
 	}
 
-	public int getWebsocketPort() {
+	/**
+	 * Gets the port that the Unity WebSocket API is running on.
+	 *
+	 * @return The port number. Typically randomly generated.
+	 */
+	private int getWebsocketPort() {
 		return websocketPort;
 	}
 }
