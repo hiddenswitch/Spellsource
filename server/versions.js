de1db8f2c94d1b43ae03a93f852385ddaa39af60
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-06-15 19:51:59 -0400

    Provide an API for adding new builds to the launcher

--- server/versions.js
+++ server/versions.js
@@ -3,6 +3,20 @@
  */
 import {Versions} from '../collections'
 
+SimpleRest.configure({
+    collections: []
+});
+
 Meteor.publish('versions', function () {
     return Versions.find({});
+});
+
+Meteor.methods({
+    latest(url, apiKey) {
+        if (apiKey !== Meteor.settings.apiKey) {
+            return;
+        }
+
+        Versions.insert({url: url, createdAt: new Date()});
+    }
 });
