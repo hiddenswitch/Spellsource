4e5fde01133aec00b5cb080f270fc15ee45ebb01
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-06-19 15:16:50 -0400

    Add additional SSL configuration property

--- net/src/main/java/com/hiddenswitch/proto3/net/applications/Remote.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/applications/Remote.java
@@ -9,13 +9,16 @@ import io.vertx.core.Vertx;
 public class Remote {
 	public static void main(String args[]) {
 		System.setProperty("java.net.preferIPv4Stack", "true");
+		System.setProperty("javax.net.ssl.trustStore", "/home/ubuntu/Minionate/metastone/mongostore");
+		System.setProperty("javax.net.ssl.trustStorePassword", "ilikeamiga");
+		System.setProperty("org.mongodb.async.type", "netty");
+		
 		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory
 				.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
 		root.setLevel(Level.ERROR);
 
 		Vertx vertx = Vertx.vertx();
-		System.getProperties().setProperty("javax.net.ssl.trustStore", "/home/ubuntu/Minionate/metastone/mongostore");
-		System.getProperties().setProperty("javax.net.ssl.trustStorePassword", "ilikeamiga");
+
 		Mongo.mongo().connect(vertx, "mongodb://spellsource1:9AD3uubaeIf71a4M11lPVAV2mJcbPzV1EC38Y4WF26M@aws-us-east-1-portal.9.dblayer.com:20276/production?ssl=true");
 		Minionate.minionate().deployAll(vertx, Future.future());
 	}
