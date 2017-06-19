09d9af3daa877c70029d0b0f3b69eba7747d7984
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-06-19 15:09:15 -0400

    fixprod

--- net/src/main/java/com/hiddenswitch/proto3/net/applications/Remote.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/applications/Remote.java
@@ -2,13 +2,11 @@ package com.hiddenswitch.proto3.net.applications;
 
 import ch.qos.logback.classic.Level;
 import com.hiddenswitch.minionate.Minionate;
+import com.hiddenswitch.proto3.net.util.Mongo;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 
-/**
- * Created by bberman on 11/29/16.
- */
-public class Embedded {
+public class Remote {
 	public static void main(String args[]) {
 		System.setProperty("java.net.preferIPv4Stack", "true");
 		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory
@@ -16,7 +14,9 @@ public class Embedded {
 		root.setLevel(Level.ERROR);
 
 		Vertx vertx = Vertx.vertx();
+		System.getProperties().setProperty("javax.net.ssl.trustStore", "/home/ubuntu/Minionate/metastone/mongostore");
+		System.getProperties().setProperty("javax.net.ssl.trustStorePassword", "ilikeamiga");
+		Mongo.mongo().connect(vertx, "mongodb://spellsource1:9AD3uubaeIf71a4M11lPVAV2mJcbPzV1EC38Y4WF26M@aws-us-east-1-portal.9.dblayer.com:20276/production?ssl=true");
 		Minionate.minionate().deployAll(vertx, Future.future());
 	}
 }
-
