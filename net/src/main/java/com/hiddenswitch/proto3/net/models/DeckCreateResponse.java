ce3a4efb904799791014403675ffe18f40ba66db
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-06-26 14:32:48 -0400

    Fixing discover issue

--- net/src/main/java/com/hiddenswitch/proto3/net/models/DeckCreateResponse.java
+++ net/src/main/java/com/hiddenswitch/proto3/net/models/DeckCreateResponse.java
@@ -1,23 +1,30 @@
 package com.hiddenswitch.proto3.net.models;
 
+import com.hiddenswitch.proto3.net.impl.util.InventoryRecord;
+
 import java.io.Serializable;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DeckCreateResponse implements Serializable {
 	private final String deckId;
-	private final List<String> inventoryIds;
+	private final GetCollectionResponse collection;
 
 
-	public DeckCreateResponse(String deckId, List<String> inventoryIds) {
+	public DeckCreateResponse(String deckId, GetCollectionResponse collection) {
 		this.deckId = deckId;
-		this.inventoryIds = inventoryIds;
+		this.collection = collection;
 	}
 
 	public List<String> getInventoryIds() {
-		return inventoryIds;
+		return collection.getInventoryRecords().stream().map(InventoryRecord::getId).collect(Collectors.toList());
 	}
 
 	public String getDeckId() {
 		return deckId;
+	}
+
+	public GetCollectionResponse getCollection() {
+		return collection;
 	}
 }
