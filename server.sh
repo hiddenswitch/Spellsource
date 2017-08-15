93b0c5b1da36e99d7ef954330af6c6a5d91f7bf9
doctorpangloss <benjamin.s.berman@gmail.com>
2017-08-15 05:34:34 +0000

    Updating server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+if [ -z "$STY" ]; then
+  exec screen -dm -S serverscreen /bin/bash "$0";
+fi
+./gradlew clean
 ./gradlew net:shadowJar
-screen -L java -javaagent:net/lib/quasar-core-0.7.5-jdk8.jar -cp net/build/libs/net-1.3.0-all.jar com.hiddenswitch.proto3.net.applications.Remote
+java -javaagent:net/lib/quasar-core-0.7.5-jdk8.jar -cp net/build/libs/net-1.3.0-all.jar com.hiddenswitch.proto3.net.applications.Remote
