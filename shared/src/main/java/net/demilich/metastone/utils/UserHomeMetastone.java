1e977b2946a1e1c93f6df773aae5f458d7a63afd
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-10-09 17:34:33 -0700

    Clear out last reference to crappy build config stuff

--- shared/src/main/java/net/demilich/metastone/utils/UserHomeMetastone.java
+++ shared/src/main/java/net/demilich/metastone/utils/UserHomeMetastone.java
@@ -5,52 +5,50 @@ import java.util.logging.Logger;
 
 import javax.swing.filechooser.FileSystemView;
 
-import net.demilich.metastone.BuildConfig;
-
 /**
  * Singleton data class that holds the platform specific path to the metastone user home dir.
  */
 public class UserHomeMetastone {
 
-    private static UserHomeMetastone INSTANCE;
-    private static String ENV_VAR_NAME = "USER_HOME_METASTONE";
+	private static UserHomeMetastone INSTANCE;
+	private static String ENV_VAR_NAME = "USER_HOME_METASTONE";
 
-    static {
-        String metastoneHomeDirPath = System.getenv(ENV_VAR_NAME);
-        
-        // if we dont have an ENV variable set for USER_HOME_METASTONE, then use the default user directory
-        if ((metastoneHomeDirPath == null || metastoneHomeDirPath.isEmpty())) {
-            metastoneHomeDirPath = (FileSystemView.getFileSystemView().getDefaultDirectory().getPath()
-                    + File.separator + BuildConfig.NAME).replace("\\", "\\\\");
-        }
+	static {
+		String metastoneHomeDirPath = System.getenv(ENV_VAR_NAME);
 
-    	UserHomeMetastone.init(metastoneHomeDirPath);
-    }
-    
-    
-    private String dirPath;
+		// if we dont have an ENV variable set for USER_HOME_METASTONE, then use the default user directory
+		if ((metastoneHomeDirPath == null || metastoneHomeDirPath.isEmpty())) {
+			metastoneHomeDirPath = (FileSystemView.getFileSystemView().getDefaultDirectory().getPath()
+					+ File.separator + "metastone").replace("\\", "\\\\");
+		}
 
-    private UserHomeMetastone(String path) {
-        dirPath = path;
-    }
+		UserHomeMetastone.init(metastoneHomeDirPath);
+	}
 
-    private static void init(String path) {
-        if(path == null) {
-            throw new NullPointerException("UserHomeMetastone.init(path) cannot be initialized with null!");
-        }
 
-        if (INSTANCE == null) {
-            INSTANCE = new UserHomeMetastone(path);
-        } else {
-            INSTANCE.dirPath = path;
-        }
-    }
+	private String dirPath;
 
-    public static String getPath() {
-        if (INSTANCE == null) {
-            throw new RuntimeException("UserHomeMetastone must first be initialized!");
-        }
+	private UserHomeMetastone(String path) {
+		dirPath = path;
+	}
 
-        return INSTANCE.dirPath;
-    }
+	private static void init(String path) {
+		if (path == null) {
+			throw new NullPointerException("UserHomeMetastone.init(path) cannot be initialized with null!");
+		}
+
+		if (INSTANCE == null) {
+			INSTANCE = new UserHomeMetastone(path);
+		} else {
+			INSTANCE.dirPath = path;
+		}
+	}
+
+	public static String getPath() {
+		if (INSTANCE == null) {
+			throw new RuntimeException("UserHomeMetastone must first be initialized!");
+		}
+
+		return INSTANCE.dirPath;
+	}
 }
