617aa6184b24079d4e94472c08572efabca8f85f
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-10-16 15:11:47 -0700

    Implement Malfurion the Pestilent

--- game/src/test/java/net/demilich/metastone/tests/KnightsOfTheFrozenThroneTests.java
+++ game/src/test/java/net/demilich/metastone/tests/KnightsOfTheFrozenThroneTests.java
@@ -3,30 +3,161 @@ package net.demilich.metastone.tests;
 
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.actions.ActionType;
+import net.demilich.metastone.game.actions.HeroPowerAction;
 import net.demilich.metastone.game.actions.PlayCardAction;
-import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.cards.CardCatalogue;
 import net.demilich.metastone.game.cards.ChooseBattlecryHeroCard;
-import net.demilich.metastone.game.cards.ChooseBattlecryMinionCard;
 import net.demilich.metastone.game.entities.heroes.HeroClass;
 import net.demilich.metastone.game.entities.minions.Minion;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
 public class KnightsOfTheFrozenThroneTests extends TestBase {
 
 	@Test
 	public void testMalfurionThePestilent() {
-		GameContext context = createContext(HeroClass.DRUID, HeroClass.WARRIOR);
-		Player player = context.getActivePlayer();
-		clearHand(context, player);
-		clearZone(context, player.getDeck());
-		ChooseBattlecryHeroCard malfurion = (ChooseBattlecryHeroCard) CardCatalogue.getCardById("hero_malfurion_the_pestilent");
-		context.getLogic().receiveCard(player.getId(), malfurion);
-		player.setMaxMana(7);
-		player.setMana(7);
-		// TODO: Finish testing Malfurion
-		context.getLogic().performGameAction(player.getId(), malfurion.playBothOptions());
-		Assert.assertEquals(player.getHero().getArmor(), 5);
+		BiFunction<GameContext, String, GameContext> joinHeroPowerCardId = (context, heroPowerCardId) -> {
+			Player player = context.getPlayer1();
+
+			HeroPowerAction action = context.getValidActions().stream().filter(ga -> ga.getActionType() == ActionType.HERO_POWER)
+					.map(ga -> (HeroPowerAction) ga)
+					.filter(ga -> ga.getChoiceCardId().equals(heroPowerCardId))
+					.findFirst().orElseThrow(AssertionError::new);
+
+			context.getLogic().performGameAction(player.getId(), action);
+
+			return context;
+		};
+
+		Stream<Consumer<GameContext>> heroPowerChecks = Stream.of(
+				(context) -> {
+					Assert.assertEquals(context.getPlayer1().getHero().getAttack(), 3);
+				},
+				(context) -> {
+					// Expect 5 armor + additional  3 from hero power
+					Assert.assertEquals(context.getPlayer1().getHero().getArmor(), 8);
+				}
+		);
+
+		Stream<String> heroPowers = Stream.of("hero_power_plague_lord1", "hero_power_plague_lord2");
+
+		List<Object> allChecked = zip(heroPowers, heroPowerChecks, (heroPowerCardId, heroPowerChecker) -> {
+			Stream<GameContext> contexts = Stream.of((Function<ChooseBattlecryHeroCard, PlayCardAction>) (card -> card.playOptions()[0]),
+					card -> card.playOptions()[1],
+					ChooseBattlecryHeroCard::playBothOptions)
+					.map(actionGetter -> {
+						ChooseBattlecryHeroCard malfurion = (ChooseBattlecryHeroCard) CardCatalogue.getCardById("hero_malfurion_the_pestilent");
+
+						GameContext context1 = createContext(HeroClass.DRUID, HeroClass.WARRIOR);
+						Player player = context1.getPlayer1();
+						clearHand(context1, player);
+						clearZone(context1, player.getDeck());
+
+						context1.getLogic().receiveCard(player.getId(), malfurion);
+						// Mana = 7 for Hero + 2 for the hero power
+						player.setMaxMana(9);
+						player.setMana(9);
+
+						PlayCardAction action = actionGetter.apply(malfurion);
+						context1.getLogic().performGameAction(player.getId(), action);
+						Assert.assertEquals(player.getHero().getArmor(), 5);
+
+						// Assert that the player has both choose one hero powers present
+						Assert.assertEquals(context1.getValidActions().stream()
+								.filter(ga -> ga.getActionType() == ActionType.HERO_POWER)
+								.count(), 2L);
+
+						// Try the hero power specified
+
+						return context1;
+					});
+
+			Stream<Function<GameContext, GameContext>> battlecryChecks = Stream.of((context1) -> {
+						Player player = context1.getPlayer1();
+
+						Assert.assertEquals(player.getMinions().size(), 2);
+						Assert.assertTrue(player.getMinions().stream().allMatch(m -> m.getSourceCard().getCardId().equals("token_frost_widow")));
+						return context1;
+					},
+					(context11) -> {
+						Player player = context11.getPlayer1();
+
+						Assert.assertEquals(player.getMinions().size(), 2);
+						Assert.assertTrue(player.getMinions().stream().allMatch(m -> m.getSourceCard().getCardId().equals("token_scarab_beetle")));
+						return context11;
+					},
+					(context12) -> {
+						Player player = context12.getPlayer1();
+
+						Assert.assertEquals(player.getMinions().size(), 4);
+						int scarabs = 0;
+						int frosts = 0;
+						for (Minion minion : player.getMinions()) {
+							if (minion.getSourceCard().getCardId().equals("token_frost_widow")) {
+								frosts += 1;
+							}
+							if (minion.getSourceCard().getCardId().equals("token_scarab_beetle")) {
+								scarabs += 1;
+							}
+						}
+						Assert.assertEquals(scarabs, 2);
+						Assert.assertEquals(frosts, 2);
+						return context12;
+					});
+
+			zip(contexts, battlecryChecks, (context, checker) -> checker.apply(context))
+					.map(context -> joinHeroPowerCardId.apply(context, heroPowerCardId))
+					.forEach(heroPowerChecker);
+
+			return null;
+		}).collect(toList());
+
+
+	}
+
+	public static <A, B, C> Stream<C> zip(Stream<? extends A> a,
+	                                      Stream<? extends B> b,
+	                                      BiFunction<? super A, ? super B, ? extends C> zipper) {
+		Objects.requireNonNull(zipper);
+		Spliterator<? extends A> aSpliterator = Objects.requireNonNull(a).spliterator();
+		Spliterator<? extends B> bSpliterator = Objects.requireNonNull(b).spliterator();
+
+		// Zipping looses DISTINCT and SORTED characteristics
+		int characteristics = aSpliterator.characteristics() & bSpliterator.characteristics() &
+				~(Spliterator.DISTINCT | Spliterator.SORTED);
+
+		long zipSize = ((characteristics & Spliterator.SIZED) != 0)
+				? Math.min(aSpliterator.getExactSizeIfKnown(), bSpliterator.getExactSizeIfKnown())
+				: -1;
+
+		Iterator<A> aIterator = Spliterators.iterator(aSpliterator);
+		Iterator<B> bIterator = Spliterators.iterator(bSpliterator);
+		Iterator<C> cIterator = new Iterator<C>() {
+			@Override
+			public boolean hasNext() {
+				return aIterator.hasNext() && bIterator.hasNext();
+			}
+
+			@Override
+			public C next() {
+				return zipper.apply(aIterator.next(), bIterator.next());
+			}
+		};
+
+		Spliterator<C> split = Spliterators.spliterator(cIterator, zipSize, characteristics);
+		return (a.isParallel() || b.isParallel())
+				? StreamSupport.stream(split, true)
+				: StreamSupport.stream(split, false);
 	}
 }
