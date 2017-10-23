eb85e8131e1869c40e5aa22f6f0ec5023546bcb6
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-10-22 17:56:14 -0700

    Validate Living mana in Journey to Ungoro

--- game/src/test/java/net/demilich/metastone/tests/KnightsOfTheFrozenThroneTests.java
+++ game/src/test/java/net/demilich/metastone/tests/KnightsOfTheFrozenThroneTests.java
@@ -7,7 +7,6 @@ import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.actions.*;
 import net.demilich.metastone.game.cards.*;
 import net.demilich.metastone.game.entities.Entity;
-import net.demilich.metastone.game.entities.heroes.Hero;
 import net.demilich.metastone.game.entities.heroes.HeroClass;
 import net.demilich.metastone.game.entities.minions.Minion;
 import net.demilich.metastone.game.entities.weapons.Weapon;
@@ -21,7 +20,6 @@ import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
@@ -529,42 +527,5 @@ public class KnightsOfTheFrozenThroneTests extends TestBase {
 
 			return null;
 		}).collect(toList());
-
-
-	}
-
-	public static <A, B, C> Stream<C> zip(Stream<? extends A> a,
-	                                      Stream<? extends B> b,
-	                                      BiFunction<? super A, ? super B, ? extends C> zipper) {
-		Objects.requireNonNull(zipper);
-		Spliterator<? extends A> aSpliterator = Objects.requireNonNull(a).spliterator();
-		Spliterator<? extends B> bSpliterator = Objects.requireNonNull(b).spliterator();
-
-		// Zipping looses DISTINCT and SORTED characteristics
-		int characteristics = aSpliterator.characteristics() & bSpliterator.characteristics() &
-				~(Spliterator.DISTINCT | Spliterator.SORTED);
-
-		long zipSize = ((characteristics & Spliterator.SIZED) != 0)
-				? Math.min(aSpliterator.getExactSizeIfKnown(), bSpliterator.getExactSizeIfKnown())
-				: -1;
-
-		Iterator<A> aIterator = Spliterators.iterator(aSpliterator);
-		Iterator<B> bIterator = Spliterators.iterator(bSpliterator);
-		Iterator<C> cIterator = new Iterator<C>() {
-			@Override
-			public boolean hasNext() {
-				return aIterator.hasNext() && bIterator.hasNext();
-			}
-
-			@Override
-			public C next() {
-				return zipper.apply(aIterator.next(), bIterator.next());
-			}
-		};
-
-		Spliterator<C> split = Spliterators.spliterator(cIterator, zipSize, characteristics);
-		return (a.isParallel() || b.isParallel())
-				? StreamSupport.stream(split, true)
-				: StreamSupport.stream(split, false);
 	}
 }
--- game/src/test/java/net/demilich/metastone/tests/TestBase.java
+++ game/src/test/java/net/demilich/metastone/tests/TestBase.java
@@ -1,8 +1,9 @@
 package net.demilich.metastone.tests;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import net.demilich.metastone.game.entities.EntityZone;
 import net.demilich.metastone.game.targeting.Zones;
@@ -55,6 +56,41 @@ public class TestBase {
 				entity.moveOrAddTo(context, Zones.REMOVED_FROM_PLAY);
 			}
 		}
+	}
+
+	public static <A, B, C> Stream<C> zip(Stream<? extends A> a,
+	                                      Stream<? extends B> b,
+	                                      BiFunction<? super A, ? super B, ? extends C> zipper) {
+		Objects.requireNonNull(zipper);
+		Spliterator<? extends A> aSpliterator = Objects.requireNonNull(a).spliterator();
+		Spliterator<? extends B> bSpliterator = Objects.requireNonNull(b).spliterator();
+
+		// Zipping looses DISTINCT and SORTED characteristics
+		int characteristics = aSpliterator.characteristics() & bSpliterator.characteristics() &
+				~(Spliterator.DISTINCT | Spliterator.SORTED);
+
+		long zipSize = ((characteristics & Spliterator.SIZED) != 0)
+				? Math.min(aSpliterator.getExactSizeIfKnown(), bSpliterator.getExactSizeIfKnown())
+				: -1;
+
+		Iterator<A> aIterator = Spliterators.iterator(aSpliterator);
+		Iterator<B> bIterator = Spliterators.iterator(bSpliterator);
+		Iterator<C> cIterator = new Iterator<C>() {
+			@Override
+			public boolean hasNext() {
+				return aIterator.hasNext() && bIterator.hasNext();
+			}
+
+			@Override
+			public C next() {
+				return zipper.apply(aIterator.next(), bIterator.next());
+			}
+		};
+
+		Spliterator<C> split = Spliterators.spliterator(cIterator, zipSize, characteristics);
+		return (a.isParallel() || b.isParallel())
+				? StreamSupport.stream(split, true)
+				: StreamSupport.stream(split, false);
 	}
 
 	protected static class TestBehaviour extends AbstractBehaviour {
