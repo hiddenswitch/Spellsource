7b59c4eedb94202fbae2a37497fd4a0511674d24
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-10-30 18:09:01 -0700

    Remove old JavaFX application code

--- app/src/main/java/net/demilich/metastone/gui/sandboxmode/ICardCollectionEditingListener.java
+++
@@ -1,9 +0,0 @@
-package net.demilich.metastone.gui.sandboxmode;
-
-import net.demilich.metastone.game.cards.CardList;
-
-public interface ICardCollectionEditingListener {
-
-	void onFinishedEditing(CardList cardList);
-
-}
--- app/src/main/java/net/demilich/metastone/tools/SpellDescSerializer.java
+++
@@ -1,4 +0,0 @@
-package net.demilich.metastone.tools;
-
-public class SpellDescSerializer extends net.demilich.metastone.game.cards.desc.SpellDescSerializer {
-}
