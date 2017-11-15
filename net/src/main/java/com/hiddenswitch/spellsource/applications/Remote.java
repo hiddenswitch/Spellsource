86bfb83bc3eca6324bcbcd6926c9e8ed8875dc00
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-11-14 17:54:56 -0800

    Enable production mongo settings

--- net/src/main/java/com/hiddenswitch/spellsource/applications/Remote.java
+++ net/src/main/java/com/hiddenswitch/spellsource/applications/Remote.java
@@ -14,8 +14,6 @@ import java.time.temporal.ChronoUnit;
 public class Remote {
 	public static void main(String args[]) {
 		System.setProperty("java.net.preferIPv4Stack", "true");
-		System.setProperty("javax.net.ssl.trustStore", "./mongostore");
-		System.setProperty("javax.net.ssl.trustStorePassword", "ilikeamiga");
 		System.setProperty("org.mongodb.async.type", "netty");
 
 		ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory
@@ -29,9 +27,11 @@ public class Remote {
 				.setWarningExceptionTime(nanos)
 				.setMaxEventLoopExecuteTime(nanos)
 				.setMaxWorkerExecuteTime(nanos)
-				.setWorkerPoolSize(Runtime.getRuntime().availableProcessors() * 10));
+				.setWorkerPoolSize(Runtime.getRuntime().availableProcessors() * 40));
 
-		Mongo.mongo().connect(vertx, "mongodb://spellsource1:9AD3uubaeIf71a4M11lPVAV2mJcbPzV1EC38Y4WF26M@aws-us-east-1-portal.9.dblayer.com:20276/production?ssl=true");
+		root.error(System.getenv("MONGO_URL"));
+
+		Mongo.mongo().connectWithEnvironment(vertx);
 		Spellsource.Spellsource().migrate(vertx, then -> {
 			if (then.failed()) {
 				root.error("Migration failed: " + then.cause().getMessage());
