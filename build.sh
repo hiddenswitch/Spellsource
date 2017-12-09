123305e9421f3d924f569e2fbff3db88dcddb745
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-12-09 03:08:55 -0500

    Streamline deployment scripts

--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ set -e
 # Build the server
 ./gradlew clean
 ./gradlew net:shadowJar
+
+# Package the two necessary files into the right places into a zip file
+zip artifact.zip \
+    ./Dockerfile \
+    ./Dockerrun.aws.json \
+    ./net/build/libs/net-1.3.0-all.jar \
+    ./net/lib/quasar-core-0.7.9-jdk8.jar \
+    ./server.sh
