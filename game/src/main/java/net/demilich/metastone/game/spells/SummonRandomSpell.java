92892ba8b6aaecea6176448e48be34b81f140996
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-12-22 23:50:33 -0500

    Retire SummonRandomSpell

--- game/src/main/java/net/demilich/metastone/game/spells/SummonRandomSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/SummonRandomSpell.java
@@ -11,21 +11,6 @@ import net.demilich.metastone.game.entities.Entity;
 import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 
-public class SummonRandomSpell extends Spell {
-
-	public static SpellDesc create(MinionCard... minionCards) {
-		Map<SpellArg, Object> arguments = SpellDesc.build(SummonRandomSpell.class);
-		arguments.put(SpellArg.CARDS, minionCards);
-		return new SpellDesc(arguments);
-	}
-
-	@Override
-	@Suspendable
-	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
-		String[] minionCardsId = (String[]) desc.get(SpellArg.CARDS);
-		String randomMinionId = context.getLogic().getRandom(Arrays.asList(minionCardsId));
-		MinionCard randomMinionCard = (MinionCard) context.getCardById(randomMinionId);
-		context.getLogic().summon(player.getId(), randomMinionCard.summon(), null, -1, false);
-	}
-
+@Deprecated
+public class SummonRandomSpell extends SummonSpell {
 }
