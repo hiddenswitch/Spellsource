9c16258a8da656a89a007f14521b9067c1a36231
Benjamin Berman <benjamin.s.berman@gmail.com>
2017-12-22 23:39:43 -0500

    Retire Summon One One copies

--- game/src/main/java/net/demilich/metastone/game/spells/custom/SummonOneOneCopiesSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/custom/SummonOneOneCopiesSpell.java
@@ -14,6 +14,7 @@ import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 import net.demilich.metastone.game.targeting.EntityReference;
 
+@Deprecated
 public class SummonOneOneCopiesSpell extends Spell {
 
 	public static SpellDesc create() {
