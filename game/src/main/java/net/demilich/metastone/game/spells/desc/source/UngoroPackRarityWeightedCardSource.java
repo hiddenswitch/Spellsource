baed6a81039a3a42c966037c0e1a044dfbca1d17
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-01-27 18:20:10 -0800

    Fix #108 don't give quests in the Un'Goro pack, improve the weighted card distributions

---
+++ game/src/main/java/net/demilich/metastone/game/spells/desc/source/UngoroPackRarityWeightedCardSource.java
@@ -0,0 +1,54 @@
+package net.demilich.metastone.game.spells.desc.source;
+
+import net.demilich.metastone.game.GameContext;
+import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.cards.*;
+import net.demilich.metastone.game.entities.heroes.HeroClass;
+
+import java.util.stream.Collectors;
+
+public class UngoroPackRarityWeightedCardSource extends CatalogueSource {
+
+	public UngoroPackRarityWeightedCardSource(SourceDesc desc) {
+		super(desc);
+	}
+
+	@Override
+	protected CardList match(GameContext context, Player player) {
+		return CardCatalogue
+				.query(context.getDeckFormat()).stream()
+				.filter(c -> c.getCardSet() == CardSet.JOURNEY_TO_UNGORO)
+				.collect(Collectors.toCollection(CardArrayList::new));
+	}
+
+	/**
+	 * Gets the weight for the Un'Goro pack that Elise the Trailblazer's token gives you.
+	 * <p>
+	 * The values here are very eyeballed.
+	 *
+	 * @param targetPlayer The player to calculate this with respect to.
+	 * @param card         The card.
+	 * @return The weight.
+	 */
+	@Override
+	public int getWeight(Player targetPlayer, Card card) {
+		boolean isClassCard = card.hasHeroClass(targetPlayer.getHero().getHeroClass());
+		int isSameClassMultiplier = isClassCard ? 4 : 1;
+		boolean isNeutral = card.hasHeroClass(HeroClass.ANY);
+
+		if (!isClassCard && !isNeutral) {
+			return 0;
+		}
+
+		switch (card.getRarity()) {
+			case LEGENDARY:
+			case EPIC:
+				return isSameClassMultiplier;
+			default:
+			case RARE:
+			case FREE:
+			case COMMON:
+				return 4 * isSameClassMultiplier;
+		}
+	}
+}
