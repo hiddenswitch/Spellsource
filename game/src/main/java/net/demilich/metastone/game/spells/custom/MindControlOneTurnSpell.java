5a6e8b5c930931acf76385f583d5115ec1b69cf8
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-03-19 18:32:38 -0700

    Add Kobold Hypnotizer

--- game/src/main/java/net/demilich/metastone/game/spells/custom/MindControlOneTurnSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/custom/MindControlOneTurnSpell.java
@@ -3,13 +3,12 @@ package net.demilich.metastone.game.spells.custom;
 import java.util.Map;
 
 import co.paralleluniverse.fibers.Suspendable;
+import net.demilich.metastone.game.spells.*;
 import net.demilich.metastone.game.utils.Attribute;
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.entities.Actor;
 import net.demilich.metastone.game.entities.Entity;
-import net.demilich.metastone.game.spells.MindControlSpell;
-import net.demilich.metastone.game.spells.TargetPlayer;
 import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 import net.demilich.metastone.game.spells.desc.trigger.EventTriggerDesc;
@@ -27,7 +26,9 @@ public class MindControlOneTurnSpell extends MindControlSpell {
 
 	public static SpellDesc create(EntityReference target) {
 		Map<SpellArg, Object> arguments = new SpellDesc(MindControlOneTurnSpell.class);
-		arguments.put(SpellArg.TARGET, target);
+		if (target != null) {
+			arguments.put(SpellArg.TARGET, target);
+		}
 		return new SpellDesc(arguments);
 	}
 
@@ -36,22 +37,35 @@ public class MindControlOneTurnSpell extends MindControlSpell {
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
 		// mind control minion
 		super.onCast(context, player, desc, source, target);
-		
+
 		if (target.isDestroyed()) {
 			return;
 		}
 
-		// minion should be able to attack this turn
-		target.getAttributes().remove(Attribute.SUMMONING_SICKNESS);
-		context.getLogic().refreshAttacksPerRound((Actor) target);
+		SpellDesc effectSpell = (SpellDesc) desc.get(SpellArg.SPELL);
+		if (effectSpell == null) {
+			// Minion should be able to attack this turn
+			effectSpell = SpellDesc.join(
+					RemoveAttributeSpell.create(target.getReference(), Attribute.SUMMONING_SICKNESS),
+					RefreshAttacksSpell.create(target.getReference()));
+		}
+
 
 		// mind control is terminated either when silenced or turn ends
-		SpellDesc reverseMindcontrolSpell = MindControlSpell.create(EntityReference.SELF, TargetPlayer.OPPONENT, false);
 		EventTrigger silenceTrigger = new SilenceTrigger(new EventTriggerDesc(SilenceTrigger.class));
-		EventTrigger turnEndTrigger = new TurnEndTrigger(new EventTriggerDesc(TurnEndTrigger.class));
-		turnEndTrigger.setOwner(player.getId());
-		Enchantment returnOnSilence = new Enchantment(silenceTrigger, turnEndTrigger, reverseMindcontrolSpell, true);
+		SpellDesc reverseMindcontrolSpell = MindControlSpell.create(EntityReference.SELF, TargetPlayer.OPPONENT, false);
+		EventTriggerDesc revertTrigger = (EventTriggerDesc) desc.get(SpellArg.REVERT_TRIGGER);
+		if (revertTrigger == null) {
+			revertTrigger = TurnEndTrigger.create(TargetPlayer.BOTH);
+		}
+
+		EventTrigger eventTrigger = revertTrigger.create();
+		eventTrigger.setOwner(player.getId());
+		Enchantment returnOnSilence = new Enchantment(silenceTrigger, eventTrigger, reverseMindcontrolSpell, true);
 		context.getLogic().addGameEventListener(player, returnOnSilence, target);
+
+		// Apply the effects
+		SpellUtils.castChildSpell(context, player, effectSpell, source, target);
 	}
 
 }
