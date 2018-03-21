5bc299c4e5c185885fce2d9285aef9f30e99a71a
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-03-21 16:39:50 -0700

    BuffAura documentation

--- game/src/main/java/net/demilich/metastone/game/spells/aura/BuffAura.java
+++ game/src/main/java/net/demilich/metastone/game/spells/aura/BuffAura.java
@@ -10,6 +10,17 @@ import net.demilich.metastone.game.spells.desc.valueprovider.AlgebraicValueProvi
 import net.demilich.metastone.game.spells.trigger.WillEndSequenceTrigger;
 import net.demilich.metastone.game.targeting.EntityReference;
 
+/**
+ * A buff aura applies the specified {@link AuraArg#ATTACK_BONUS} and {@link AuraArg#HP_BONUS} to the target entities as
+ * long as the {@link AuraArg#CONDITION} is satisfied.
+ * <p>
+ * To support reevaluating the condition at different times, optionall specify an {@link AuraArg#SECONDARY_TRIGGER} that
+ * fires when the condition ought to change. Otherwise, the condition is evaluated at the same time auras are, which is
+ * whenever the board changes or a sequence ends.
+ * <p>
+ * The underlying attributes used for this buff are {@link net.demilich.metastone.game.utils.Attribute#AURA_ATTACK_BONUS}
+ * and {@link net.demilich.metastone.game.utils.Attribute#AURA_HP_BONUS}.
+ */
 public class BuffAura extends Aura {
 
 	public BuffAura(AuraDesc desc) {
