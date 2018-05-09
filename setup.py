84980345514cab7d5c1fa5bf974e9887d00086de
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-05-09 12:02:10 -0700

    Finish Python release

--- setup.py
+++ setup.py
@@ -22,8 +22,53 @@ except Exception as ex:
     raise ex
 
 setup(name='spellsource',
-      version='0.2.0',
+      version='0.2.1',
       description='The Spellsource card game engine, supports Hearthstone AI and simulation',
+      long_description='''
+A multiplayer, networked adaptation of ``metastone``. This is a
+simulator and game server for community and official Hearthstone cards.
+
+Download the Hidden Switch Launcher for `Mac OS
+X <http://go.hiddenswitch.com/mac>`__ or
+`Windows <http://go.hiddenswitch.com/win>`__ to get a copy of the game
+client. Play online against others! No other installation required.
+
+Quick Start Python
+~~~~~~~~~~~~~~~~~~
+
+The ``spellsource`` package creates a bridge with the Java-based
+``Spellsource-Server`` engine. It provides a direct 1-to-1 mapping with
+the Java API.
+
+1. Install the Java 8 SDK (JDK) from Oracle’s website.
+2. ``pip install spellsource`` to install the latest version of the
+   package.
+3. Start a game and play it with the specified bots:
+
+   .. code:: python
+
+      from spellsource.context import Context
+      from spellsource.playrandombehaviour import PlayRandomBehaviour
+
+      with Context() as ctx:
+          game_context = ctx.game.GameContext.fromTwoRandomDecks()
+          behaviour1 = PlayRandomBehaviour()
+          behaviour2 = PlayRandomBehaviour()
+          game_context.getPlayer1().setBehaviour(behaviour1.wrap(ctx))
+          game_context.getPlayer2().setBehaviour(behaviour2.wrap(ctx))
+          game_context.play()
+          assert game_context.updateAndGetGameOver()
+
+Visit
+```GameStateValueBehaviour`` <spellsource/gamestatevaluebehaviour.py>`__
+to see an implementation of a complex AI bot in Python. This is a direct
+port of the Java code. Unfortunately, on the Python platform, remoting
+(accessing the Java engine) in the particular way this bot does is slow.
+To implement more sophisticated bots, consider adding a method to
+``GameContext`` that will extract the exact data, in a binary format,
+that you need in your Python implementation, to reduce the communication
+overhead between Java and Python.
+      ''',
       url='http://github.com/hiddenswitch/Spellsource-Server',
       python_requires='>3.6',
       author='Benjamin Berman',
@@ -44,6 +89,5 @@ setup(name='spellsource',
                    # Specify the Python versions you support here. In particular, ensure
                    # that you indicate whether you support Python 2, Python 3 or both.
                    'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.6.5'],
+                   'Programming Language :: Python :: 3.6'],
       zip_safe=False)
