3b156205f47150a84ea7463d4ae0c5f3993c195e
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-05-24 18:43:20 -0700

    Implement deflect

--- game/src/main/java/net/demilich/metastone/game/events/LoseDeflectEvent.java
+++ game/src/main/java/net/demilich/metastone/game/events/LoseDeflectEvent.java
@@ -3,10 +3,10 @@ package net.demilich.metastone.game.events;
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.entities.Entity;
 
-public final class LoseDivineShieldEvent extends GameEvent implements HasVictim {
+public final class LoseDeflectEvent extends GameEvent implements HasVictim {
 	private final Entity eventTarget;
 
-	public LoseDivineShieldEvent(GameContext context, Entity loserOfShield, int targetPlayerId, int sourcePlayerId) {
+	public LoseDeflectEvent(GameContext context, Entity loserOfShield, int targetPlayerId, int sourcePlayerId) {
 		super(context, targetPlayerId, sourcePlayerId);
 		this.eventTarget = loserOfShield;
 	}
@@ -18,7 +18,7 @@ public final class LoseDivineShieldEvent extends GameEvent implements HasVictim 
 
 	@Override
 	public GameEventType getEventType() {
-		return GameEventType.LOSE_DIVINE_SHIELD;
+		return GameEventType.LOSE_DEFLECT;
 	}
 
 	@Override
@@ -26,4 +26,3 @@ public final class LoseDivineShieldEvent extends GameEvent implements HasVictim 
 		return getEventTarget();
 	}
 }
-
--- game/src/main/java/net/demilich/metastone/game/spells/trigger/LoseDeflectTrigger.java
+++ game/src/main/java/net/demilich/metastone/game/spells/trigger/LoseDeflectTrigger.java
@@ -5,26 +5,25 @@ import net.demilich.metastone.game.entities.EntityType;
 import net.demilich.metastone.game.events.GameEvent;
 import net.demilich.metastone.game.events.GameEventType;
 import net.demilich.metastone.game.events.LoseDeflectEvent;
-import net.demilich.metastone.game.events.LoseDivineShieldEvent;
 import net.demilich.metastone.game.spells.desc.trigger.EventTriggerArg;
 import net.demilich.metastone.game.spells.desc.trigger.EventTriggerDesc;
 
-public class LoseDivineShieldTrigger extends EventTrigger {
-	public LoseDivineShieldTrigger(EventTriggerDesc desc) {
+public class LoseDeflectTrigger extends EventTrigger {
+	public LoseDeflectTrigger(EventTriggerDesc desc) {
 		super(desc);
 	}
 
 	@Override
 	protected boolean fire(GameEvent event, Entity host) {
-		LoseDivineShieldEvent loseDivineShieldEvent = (LoseDivineShieldEvent) event;
+		LoseDeflectEvent loseDeflectEvent = (LoseDeflectEvent) event;
 
 		EntityType sourceEntityType = (EntityType) getDesc().get(EventTriggerArg.SOURCE_ENTITY_TYPE);
-		if (sourceEntityType != null && loseDivineShieldEvent.getSource().getEntityType() != sourceEntityType) {
+		if (sourceEntityType != null && loseDeflectEvent.getSource().getEntityType() != sourceEntityType) {
 			return false;
 		}
 
 		EntityType targetEntityType = (EntityType) getDesc().get(EventTriggerArg.TARGET_ENTITY_TYPE);
-		if (targetEntityType != null && loseDivineShieldEvent.getVictim().getEntityType() != targetEntityType) {
+		if (targetEntityType != null && loseDeflectEvent.getVictim().getEntityType() != targetEntityType) {
 			return false;
 		}
 
@@ -36,4 +35,3 @@ public class LoseDivineShieldTrigger extends EventTrigger {
 		return GameEventType.LOSE_DIVINE_SHIELD;
 	}
 }
-
