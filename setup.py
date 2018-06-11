24da1bd1995b144dde483a83df5cbfd9d168d506
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-06-10 21:58:56 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except Exception as ex:
     raise ex
 
 setup(name='spellsource',
-      version='0.3.4',
+      version='0.4.0',
       description='The Spellsource card game engine, supports Hearthstone AI and simulation',
       long_description='''
 A multiplayer, networked adaptation of ``metastone``. This is a
