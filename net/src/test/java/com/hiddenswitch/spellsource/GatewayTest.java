53e8c9e40ae5ac06f9fccb5d085f20fc16aa88b9
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-06-30 19:21:36 -0700

    Test resumes actions

--- net/src/test/java/com/hiddenswitch/spellsource/GatewayTest.java
+++ net/src/test/java/com/hiddenswitch/spellsource/GatewayTest.java
@@ -1,6 +1,5 @@
 package com.hiddenswitch.spellsource;
 
-import ch.qos.logback.classic.Level;
 import co.paralleluniverse.fibers.SuspendExecution;
 import co.paralleluniverse.strands.Strand;
 import com.hiddenswitch.spellsource.client.ApiClient;
@@ -12,7 +11,6 @@ import com.hiddenswitch.spellsource.concurrent.SuspendableMap;
 import com.hiddenswitch.spellsource.impl.GameId;
 import com.hiddenswitch.spellsource.impl.SpellsourceTestBase;
 import com.hiddenswitch.spellsource.impl.UserId;
-import com.hiddenswitch.spellsource.util.Logging;
 import com.hiddenswitch.spellsource.util.Sync;
 import com.hiddenswitch.spellsource.util.UnityClient;
 import io.vertx.core.CompositeFuture;
@@ -27,7 +25,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -369,5 +369,43 @@ public class GatewayTest extends SpellsourceTestBase {
 		client.waitUntilDone();
 		context.assertTrue(client.isGameOver());
 		context.assertEquals(mulligans.get(), 2);
+	}
+
+	@Test
+	public void testReconnectsResumesNormalActions(TestContext context) throws InterruptedException {
+		AtomicInteger requests = new AtomicInteger();
+		List<Integer> actions = new ArrayList<>();
+		UnityClient client = new UnityClient(context) {
+			@Override
+			protected boolean onRequestAction(ServerToClientMessage message) {
+				int reqs = requests.getAndIncrement();
+				if (reqs == 0) {
+					actions.addAll(message.getActions().getCompatibility());
+					gameOverLatch.countDown();
+					disconnect();
+					return false;
+				} else if (reqs == 1) {
+					context.assertEquals(message.getActions().getCompatibility().size(), actions.size());
+				}
+
+				return true;
+			}
+		};
+
+		client.createUserAccount();
+		client.matchmakeQuickPlay(null);
+		client.waitUntilDone();
+		Thread.sleep(100L);
+		context.assertFalse(client.isConnected());
+		sync(() -> {
+			Strand.sleep(100L);
+			// Game should still be running
+			context.assertTrue(Games.getGames().containsKey(new UserId(client.getAccount().getId())));
+			Strand.sleep(100L);
+		});
+		// Reconnect
+		client.play();
+		client.waitUntilDone();
+		context.assertTrue(client.isGameOver());
 	}
 }
