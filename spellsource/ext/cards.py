d345359bba0d4569bfef41d29fff0e75847feac6
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-08-03 10:56:18 -0700

    Improve packaging of card data to support validation

--- spellsource/ext/cards.py
+++ spellsource/ext/cards.py
@@ -2,7 +2,9 @@ from json import dump, load
 from os import walk, path
 from collections import deque
 import re
+from typing import Generator, Tuple
 
+from mypy_extensions import TypedDict
 from objdict import ObjDict as OrderedDict
 from objdict import JsonEncoder
 
@@ -21,11 +23,10 @@ CLASS_MAPPING = {
     'DREAM': 'ANY'
 }
 
-
-def iter_card_and_file_path(start_path=None):
+def iter_card_and_file_path(start_path: str = None) -> Generator[Tuple[dict, str], None, None]:
     if start_path is None:
-        start_path = path.join(path.dirname(__file__), 'src/main/resources/cards')
-    
+        start_path = path.join(path.dirname(__file__), '../../cards/src/main/resources/cards')
+
     for root, dirnames, filenames in walk(start_path):
         for filename in filenames:
             if '.json' not in filename:
@@ -40,19 +41,19 @@ def iter_card_and_file_path(start_path=None):
                     continue
 
 
-def iter_cards(start_path=None):
+def iter_cards(start_path: str = None) -> Generator[dict, None, None]:
     for card, _ in iter_card_and_file_path(start_path):
         yield card
 
 
-def walk_card(card):
+def walk_card(card: dict):
     queue = deque([(card, {}, None, card)])
-    
+
     while len(queue) > 0:
         (next_dict, parent, key, inherits) = queue.popleft()
         yield (next_dict, parent, key, inherits)
-        for k, v in next_dict.iteritems():
-            
+        for k, v in next_dict.items():
+
             if isinstance(v, dict) or isinstance(v, OrderedDict):
                 copy = inherits.copy()
                 copy.update(v)
@@ -65,7 +66,7 @@ def walk_card(card):
                         queue.append((item, next_dict, k, copy))
 
 
-def write_card(card, filepath):
+def write_card(card: dict, filepath: str):
     with open(filepath, 'w') as fp:
         dump(card, fp, indent=2, separators=(',', ': '), cls=JsonEncoder)
 
