ce074686051614901ad243a717884b7ecd018b7c
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-08-20 23:19:43 -0700

    Make a python deploy script

---
+++ deploy_python.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+source .venv/bin/activate
+rm -rf dist/
+mkdir -pv dist
+pip3 install wheel twine >/dev/null
+echo Building
+python3 setup.py sdist bdist_wheel >/dev/null
+echo Deploying
+twine upload dist/*
