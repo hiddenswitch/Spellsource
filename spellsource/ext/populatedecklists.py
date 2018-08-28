5cc96dbdb6c094b7107450906931d708cb986803
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-08-28 16:25:00 -0700

    Update deck lists

--- spellsource/ext/populatedecklists.py
+++ spellsource/ext/populatedecklists.py
@@ -45,7 +45,7 @@ _FILTER = {
 _TEMPOSTORM_URL = 'https://tempostorm.com/api/snapshots/findOne?'
 
 
-def _get_snapshot_url(snap_num=55):
+def _get_snapshot_url(snap_num=58):
     filter = copy.deepcopy(_FILTER)
     filter['where']['snapNum'] = snap_num
     filter_str = urllib.parse.urlencode({"filter": json.dumps(filter, indent=None, separators=[',', ':'])})
@@ -55,7 +55,9 @@ def _get_snapshot_url(snap_num=55):
 def write_decklists():
     request = urllib.request.Request(url=_get_snapshot_url(),
                                      headers={
-                                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7'
+                                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) '
+                                                       'AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 '
+                                                       'Safari/604.4.7'
                                      })
     print(_get_snapshot_url())
     snapshot = json.load(urllib.request.urlopen(request))
@@ -65,7 +67,9 @@ def write_decklists():
                         for card in deck['deck']['cards']]} for deck in snapshot['deckTiers']]
     for deck in decks:
         filename = deck['name'] + '.txt'
-        with open(path.join('net', 'src', 'main', 'resources', 'decklists', 'current', filename), 'w') as f:
+        with open(
+                path.join(path.dirname(__file__), '..', '..', 'net', 'src', 'main', 'resources', 'decklists', 'current',
+                          filename), 'w') as f:
             lines = ['Name: ' + deck['name'], 'Class: ' + deck['heroClass'], 'Format: Standard']
             lines += [str.format('{0}x {1}', card['quantity'], card['name']) for card in deck['cards']]
             lines = '\n'.join(lines)
