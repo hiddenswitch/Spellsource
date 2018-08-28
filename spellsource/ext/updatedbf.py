e7e9e518162352fe708e3e4f4692e0d11cc61ec1
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-08-28 13:39:45 -0700

    Fix deck list parsing for Boomsday decks

--- spellsource/ext/updatedbf.py
+++ spellsource/ext/updatedbf.py
@@ -6,7 +6,7 @@ from json import dump
 def write_dbf_json():
     dbf = cardxml.load_dbf()[0]
     new_json = {str(id): card.name for id, card in dbf.items() if card.collectible}
-    f = open(path.join(path.dirname(__file__), 'src/main/resources/dbf.json'), 'w')
+    f = open(path.join(path.dirname(__file__), '..', '..', 'game', 'src', 'main', 'resources', 'dbf.json'), 'w')
     dump(new_json, f, indent=2)
     f.close()
 
