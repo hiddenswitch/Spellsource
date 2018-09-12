7c19419bb92921b4ea368ee51be64a4626eccaf3
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-09-11 17:11:43 -0700

    Fix #565 and fix #602

--- game/src/main/java/net/demilich/metastone/game/decks/DeckFactory.java
+++ game/src/main/java/net/demilich/metastone/game/decks/DeckFactory.java
@@ -4,19 +4,12 @@ import net.demilich.metastone.game.cards.CardSet;
 import net.demilich.metastone.game.entities.heroes.HeroClass;
 import org.apache.commons.lang3.RandomUtils;
 
+import java.util.Arrays;
+
 public class DeckFactory {
 
 	public static GameDeck getRandomDeck(HeroClass heroClass, DeckFormat deckFormat) {
 		return new RandomDeck(heroClass, deckFormat);
 	}
 
-	public static Deck getRandomDeck() {
-		HeroClass[] heroClasses = {HeroClass.BROWN, HeroClass.GREEN, HeroClass.BLUE, HeroClass.GOLD, HeroClass.WHITE, HeroClass.BLACK, HeroClass.SILVER, HeroClass.VIOLET, HeroClass.RED};
-		Deck randomDeck = DeckFactory.getRandomDeck(
-				heroClasses[RandomUtils.nextInt(0, heroClasses.length)],
-				new DeckFormat().withCardSets(
-						CardSet.BASIC,
-						CardSet.CLASSIC));
-		return randomDeck;
-	}
 }
