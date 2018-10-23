852393f2f55b29747051f0502922eb759640937f
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-10-23 18:06:47 +0200

    DamageType docs

--- game/src/main/java/net/demilich/metastone/game/targeting/DamageType.java
+++ game/src/main/java/net/demilich/metastone/game/targeting/DamageType.java
@@ -1,6 +1,15 @@
 package net.demilich.metastone.game.targeting;
 
+/**
+ * Types of damage. Currently only used for rendering, but eventually could be used for gameplay.
+ */
 public enum DamageType {
+	/**
+	 * Physical damage is caused by physical attacks by actors.
+	 */
 	PHYSICAL,
+	/**
+	 * Magical damage is caused by spells and effects. It is typically rendered by missiles in the client.
+	 */
 	MAGICAL
 }
