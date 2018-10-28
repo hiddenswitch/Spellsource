d474931b65cdec2a00988299ac353463cf34a4d0
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-10-28 13:00:50 -0700

    Use an unweighted catalogue source for filters by default.

--- game/src/main/java/net/demilich/metastone/game/spells/desc/source/UnweightedCatalogueSource.java
+++ game/src/main/java/net/demilich/metastone/game/spells/desc/source/UnweightedCatalogueSource.java
@@ -6,10 +6,16 @@ import net.demilich.metastone.game.cards.CardCatalogue;
 import net.demilich.metastone.game.cards.CardList;
 import net.demilich.metastone.game.entities.Entity;
 
+import java.util.Map;
+
 public class UnweightedCatalogueSource extends CardSource implements HasCardCreationSideEffects {
 
 	public UnweightedCatalogueSource(CardSourceDesc desc) {
 		super(desc);
+	}
+
+	public static UnweightedCatalogueSource create() {
+		return new UnweightedCatalogueSource(new CardSourceDesc(UnweightedCatalogueSource.class));
 	}
 
 	@Override
