50c915ee7f4f4b7f9ebf589c19f5d99b258f7b0f
James Gale <obsidiantoolzg@gmail.com>
2018-10-27 20:45:38 -0700

    Add custom cards Jastor Gallywix, Sunlance, Whizbang the Plunderful

---
+++ game/src/main/java/net/demilich/metastone/game/spells/custom/SwapHpAndCostSpell.java
@@ -0,0 +1,28 @@
+package net.demilich.metastone.game.spells.custom;
+
+import net.demilich.metastone.game.GameContext;
+import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.cards.Card;
+import net.demilich.metastone.game.entities.Entity;
+import net.demilich.metastone.game.spells.CardCostModifierSpell;
+import net.demilich.metastone.game.spells.SetHpSpell;
+import net.demilich.metastone.game.spells.Spell;
+import net.demilich.metastone.game.spells.desc.SpellDesc;
+import net.demilich.metastone.game.spells.desc.manamodifier.CardCostModifierDesc;
+import net.demilich.metastone.game.spells.desc.valueprovider.AlgebraicOperation;
+
+public class SwapHpAndCostSpell extends Spell {
+
+
+    @Override
+    protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
+        int cost = context.getLogic().getModifiedManaCost(player, (Card) target);
+        int hp = ((Card) target).getHp();
+
+        SpellDesc changeCost = CardCostModifierSpell.create(target.getReference(), AlgebraicOperation.SET, hp);
+        SpellDesc changeHp = SetHpSpell.create(cost);
+        context.getLogic().castSpell(player.getId(), changeCost, source.getReference(), target.getReference(), true);
+        context.getLogic().castSpell(player.getId(), changeHp, source.getReference(), target.getReference(), true);
+
+    }
+}
