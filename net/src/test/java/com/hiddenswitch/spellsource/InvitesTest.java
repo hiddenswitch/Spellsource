16022ee5a2db17a20e1012e7e72008ae0018527d
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-11-08 15:43:08 -0800

    Friend rejected correctly

--- net/src/test/java/com/hiddenswitch/spellsource/InvitesTest.java
+++ net/src/test/java/com/hiddenswitch/spellsource/InvitesTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static com.github.fromage.quasi.strands.Strand.sleep;
 import static com.hiddenswitch.spellsource.util.Sync.invoke;
 import static org.junit.Assert.*;
 
@@ -83,8 +84,86 @@ public class InvitesTest extends SpellsourceTestBase {
 			assertEquals(Invite.StatusEnum.UNDELIVERED, inviteResponse.getInvite().getStatus());
 			friended.await();
 			didReceiveFriend.await();
-			Strand.sleep(1000L);
+			sleep(1000L);
 			assertEquals(0, inviteChecks.get());
+
+			GetAccountsResponse updatedRecipient = invoke(recipient.getApi()::getAccount, recipient.getUserId().toString());
+			GetAccountsResponse updatedSender = invoke(sender.getApi()::getAccount, sender.getUserId().toString());
+			assertEquals(1, updatedRecipient.getAccounts().get(0).getFriends().size());
+			assertEquals(1, updatedSender.getAccounts().get(0).getFriends().size());
+			assertEquals(sender.getUserId().toString(), updatedRecipient.getAccounts().get(0).getFriends().get(0).getFriendId());
+			assertEquals(recipient.getUserId().toString(), updatedSender.getAccounts().get(0).getFriends().get(0).getFriendId());
+		});
+	}
+
+	@Test
+	@Suspendable
+	public void testSenderRejectsFriend(TestContext testContext) {
+		sync(() -> {
+			CountDownLatch rejected = new CountDownLatch(1);
+			AtomicInteger inviteChecks = new AtomicInteger(2);
+			AtomicReference<String> recipientId = new AtomicReference<>();
+			UnityClient sender = new UnityClient(testContext) {
+				@Override
+				protected void handleMessage(Envelope env) {
+					if (env.getAdded() != null && env.getAdded().getFriend() != null) {
+						fail("Should not have friended");
+					}
+
+					if (env.getAdded() != null && env.getAdded().getInvite() != null) {
+						Invite invite = env.getAdded().getInvite();
+						// Confirm the invite statuses are updated correctly
+						if (rejected.getCount() == 1L) {
+							assertEquals(Invite.StatusEnum.UNDELIVERED, invite.getStatus());
+							inviteChecks.decrementAndGet();
+						} else if (rejected.getCount() == 0L) {
+							assertEquals(Invite.StatusEnum.REJECTED, invite.getStatus());
+							inviteChecks.decrementAndGet();
+						}
+					}
+				}
+			};
+			sender.ensureConnected();
+
+			UnityClient recipient = new UnityClient(testContext) {
+				@Override
+				protected void handleMessage(Envelope env) {
+					if (env.getAdded() != null && env.getAdded().getInvite() != null) {
+						Invite invite = env.getAdded().getInvite();
+						try {
+							if (invite.getFriendId() != null) {
+								assertEquals(Invite.StatusEnum.PENDING, invite.getStatus());
+								this.getApi().deleteInvite(invite.getId());
+								rejected.countDown();
+							}
+						} catch (Exception ex) {
+							fail(ex.getMessage());
+						}
+					}
+				}
+			};
+			recipient.ensureConnected();
+
+			sender.createUserAccount();
+			recipient.createUserAccount();
+			recipientId.set(recipient.getUserId().toString());
+			assertTrue(recipient.getAccount().getName().contains("#"));
+
+			InviteResponse inviteResponse = invoke(sender.getApi()::postInvite, new InvitePostRequest()
+					.friend(true)
+					// Get name contains the privacy token
+					.toUserNameWithToken(recipient.getAccount().getName())
+					.message("Would you be my friend?"));
+
+			assertEquals(Invite.StatusEnum.UNDELIVERED, inviteResponse.getInvite().getStatus());
+			rejected.await();
+			sleep(1000L);
+			assertEquals(0, inviteChecks.get());
+
+			GetAccountsResponse updatedRecipient = invoke(recipient.getApi()::getAccount, recipient.getUserId().toString());
+			GetAccountsResponse updatedSender = invoke(sender.getApi()::getAccount, sender.getUserId().toString());
+			assertEquals(0, updatedRecipient.getAccounts().get(0).getFriends().size());
+			assertEquals(0, updatedSender.getAccounts().get(0).getFriends().size());
 		});
 	}
 }
