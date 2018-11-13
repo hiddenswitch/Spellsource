4864a0044fa2ffd48b91db86fbd03d9c89ae0c20
James Gale <obsidiantoolzg@gmail.com>
2018-11-13 09:40:35 -0800

    Add OverkillSpell documentation

--- game/src/main/java/net/demilich/metastone/game/spells/OverkillSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/OverkillSpell.java
@@ -7,11 +7,48 @@ import net.demilich.metastone.game.entities.minions.Minion;
 import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 
+/**
+ * This spell implements the new Overkill mechanic introduced in Rastakhan's Rumble
+ * It's meant to happen when something kills a minion by dealing more damage than it has health
+ * <p>
+ * The spell can serve both as a {@link DamageSpell} if it has a {@link SpellArg#VALUE} in its spelldesc,
+ * or just simply as a way to simply check the state of a minion and fire the spell appropriately
+ * <p>
+ *
+ * <b>Example:</b> on a minion with "Overkill: Draw 2 Cards"
+ * "trigger": {
+ *     "eventTrigger": {
+ *       "class": "AfterPhysicalAttackTrigger",
+ *       "hostTargetType": "IGNORE_OTHER_SOURCES",
+ *       "targetEntityType": "MINION"
+ *     },
+ *     "spell": {
+ *       "class": "OverkillSpell",
+ *       "target": "EVENT_TARGET",
+ *       "spell": {
+ *         "class": "DrawCardSpell",
+ *         "value": 2
+ *       }
+ *     }
+ *   },
+ *
+ * <b>Example:</b> on a Spell with "Deal 3 damage. Overkill: Summon a 5/5 Devilsaur."
+ * "spell": {
+ *     "class": "OverkillSpell",
+ *     "value": 3,
+ *     "spell": {
+ *       "class": "SummonSpell",
+ *       "card": "token_devilsaur"
+ *     }
+ *   },
+ */
+
 public class OverkillSpell extends DamageSpell {
 
     @Override
     protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
         if (desc.containsKey(SpellArg.VALUE)) {
+            //allow the OverkillSpell to serve as a damage spell as well
             super.onCast(context, player, desc, source, target);
         }
         if (target instanceof Minion) {
