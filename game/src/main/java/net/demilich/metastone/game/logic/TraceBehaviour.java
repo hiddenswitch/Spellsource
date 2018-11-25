e55e7d47bf60379173994ccdea4bc43989913360
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-11-25 11:56:16 -0800

    Making trace test more rigorous, fixing formatting, all tests pass

--- game/src/main/java/net/demilich/metastone/game/logic/TraceBehaviour.java
+++ game/src/main/java/net/demilich/metastone/game/logic/TraceBehaviour.java
@@ -5,6 +5,7 @@ import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.actions.GameAction;
 import net.demilich.metastone.game.behaviour.UtilityBehaviour;
 import net.demilich.metastone.game.cards.Card;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,30 +17,30 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 class TraceBehaviour extends UtilityBehaviour {
-	private int playerId;
-	private int[][] mulligans;
-	private AtomicInteger nextAction;
-	private List<Integer> actions;
-	private Optional<Consumer<GameContext>> recorder;
+	private final int playerId;
+	private final int[][] mulligans;
+	private final AtomicInteger nextAction;
+	private final List<Integer> actions;
+	private final Consumer<GameContext> recorder;
 
 	/**
-	 * @param playerId The player that this behaviour is "playing" for.
-	 * @param mulligans The list of mulligans from the traced game.
+	 * @param playerId   The player that this behaviour is "playing" for.
+	 * @param mulligans  The list of mulligans from the traced game.
 	 * @param nextAction The index of the next action to take (shared between two {@link TraceBehaviour}s).
-	 * @param actions The list of action (indices) from the traced game.
-	 * @param recorder [Optional] consumer to be called on every {@link GameContext} before each action is taken.
+	 * @param actions    The list of action (indices) from the traced game.
+	 * @param beforeRequestActionHandler   [Optional] consumer to be called on every {@link GameContext} before each action is taken.
 	 */
 	TraceBehaviour(
 			int playerId,
 			int[][] mulligans,
 			AtomicInteger nextAction,
 			List<Integer> actions,
-			Optional<Consumer<GameContext>> recorder) {
+			@Nullable Consumer<GameContext> beforeRequestActionHandler) {
 		this.actions = actions;
 		this.mulligans = mulligans;
 		this.nextAction = nextAction;
 		this.playerId = playerId;
-		this.recorder = recorder;
+		this.recorder = beforeRequestActionHandler;
 	}
 
 	@Override
@@ -50,8 +51,8 @@ class TraceBehaviour extends UtilityBehaviour {
 	@Override
 	@SuppressWarnings("unchecked")
 	public List<Card> mulligan(GameContext context, Player player, List<Card> cards) {
-		if (recorder.isPresent()) {
-			recorder.get().accept(context);
+		if (recorder != null) {
+			recorder.accept(context);
 		}
 		if (playerId != player.getId()) {
 			return Collections.emptyList();
@@ -66,16 +67,16 @@ class TraceBehaviour extends UtilityBehaviour {
 					if (card.isPresent()) {
 						return card.get();
 					} else {
-                        throw new NullPointerException();
-                    }
+						throw new NullPointerException();
+					}
 				})
 				.collect(Collectors.toList());
 	}
 
 	@Override
 	public GameAction requestAction(GameContext context, Player player, List<GameAction> validActions) {
-		if (recorder.isPresent()) {
-			recorder.get().accept(context);
+		if (recorder != null) {
+			recorder.accept(context);
 		}
 		if (playerId != player.getId()) {
 			return null;
