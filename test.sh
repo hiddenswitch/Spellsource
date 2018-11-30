dad1eb0311b95f6751ce0232111a2fc6e0f368c5
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-11-30 13:21:32 -0800

    Further tweak test script

--- test.sh
+++ test.sh
@@ -7,34 +7,34 @@ echo "Clearing database"
 mongo testdb --eval "printjson(db.dropDatabase())" > /dev/null
 echo "Running tests"
 
-if [ "$CI" = "true" ] ; then
+if [[ "$CI" = "true" ]] ; then
   export GRADLE_CMD="./gradlew"
 else
   export GRADLE_CMD=gradle
 fi
 
-$GRADLE_CMD cards:test
-$GRADLE_CMD game:test
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.AccountsTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.BotsTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.BroadcastTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.CardsTest
+${GRADLE_CMD} cards:test
+${GRADLE_CMD} game:test
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.AccountsTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.BotsTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.BroadcastTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.CardsTest
 # This is too spotty on automation, even though it does pass.
-# $GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.ClusterTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.ConnectionTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.ConversationTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.DeckParsingTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.DeckTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.DraftTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.FriendTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.GamesTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.GatewayTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.InventoryTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.InvitesTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.LogicTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.ModelsTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.PersistenceTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.PythonBridgeTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.ReplayTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.SimultaneousGamesTest
-$GRADLE_CMD net:test --tests=com.hiddenswitch.spellsource.TypeTest
+# ${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.ClusterTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.ConnectionTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.ConversationTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.DeckParsingTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.DeckTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.DraftTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.FriendTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.GamesTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.GatewayTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.InventoryTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.InvitesTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.LogicTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.ModelsTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.PersistenceTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.PythonBridgeTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.ReplayTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.SimultaneousGamesTest
+${GRADLE_CMD} net:test --tests=com.hiddenswitch.spellsource.TypeTest
