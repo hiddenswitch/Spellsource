0cca710c3ea05781252b8d351ce9a0212b35c2b1
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-12-09 16:07:33 -0800

    Fix #739

--- game/src/test/java/com/blizzard/hearthstone/LeagueOfExplorersTests.java
+++ game/src/test/java/com/blizzard/hearthstone/LeagueOfExplorersTests.java
@@ -7,6 +7,7 @@ import net.demilich.metastone.game.actions.DiscoverAction;
 import net.demilich.metastone.game.actions.GameAction;
 import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.cards.CardCatalogue;
+import net.demilich.metastone.game.decks.FixedCardsDeckFormat;
 import net.demilich.metastone.game.entities.heroes.HeroClass;
 import net.demilich.metastone.game.entities.minions.Minion;
 import net.demilich.metastone.game.spells.SpellUtils;
@@ -15,6 +16,7 @@ import net.demilich.metastone.tests.util.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -34,6 +36,62 @@ public class LeagueOfExplorersTests extends TestBase {
 			playCard(context, player, "minion_reno_jackson");
 			assertEquals(player.getHero().getHp(), 30);
 		});
+
+		// Test a specific deck from a user
+		{
+			String deckListReno = "### Full Legend Paladin\n" +
+					"# Class: GOLD\n" +
+					"# Format: Custom\n" +
+					"#\n" +
+					"# 1x (1) Sir Finley Mrrgglton\n" +
+					"# 1x (2) Prince Keleseth\n" +
+					"# 1x (3) Moroes\n" +
+					"# 1x (3) Wickerflame Burnbristle\n" +
+					"# 1x (3) Zola the Gorgon\n" +
+					"# 1x (4) Spiritsinger Umbra\n" +
+					"# 1x (5) Elise the Trailblazer\n" +
+					"# 1x (5) Feugen\n" +
+					"# 1x (5) Nexus-Champion Saraad\n" +
+					"# 1x (5) Prince Malchezaar\n" +
+					"# 1x (5) Stalagg\n" +
+					"# 1x (5) Unnerfed Sylvanas Windrunner\n" +
+					"# 1x (6) Cairne Bloodhoof\n" +
+					"# 1x (6) Emperor Thaurissan\n" +
+					"# 1x (6) Justicar Trueheart\n" +
+					"# 1x (6) Reno Jackson\n" +
+					"# 1x (6) Sunkeeper Tarim\n" +
+					"# 1x (6) Val'anyr\n" +
+					"# 1x (7) Dr. Boom\n" +
+					"# 1x (7) Nexus-King Salhadaar\n" +
+					"# 1x (8) Kel'Thuzad\n" +
+					"# 1x (8) Ragnaros the Firelord\n" +
+					"# 1x (8) The Lich King\n" +
+					"# 1x (8) Tirion Fordring\n" +
+					"# 1x (9) King Krush\n" +
+					"# 1x (9) Uther of the Ebon Blade\n" +
+					"# 1x (9) Ysera\n" +
+					"# 1x (10) N'Zoth, the Corruptor\n" +
+					"# 1x (10) Y'Shaarj, Rage Unbound\n" +
+					"# 1x (30) Rise of the Ancient Ones";
+
+			GameContext context = GameContext.fromDeckLists(Arrays.asList(deckListReno, deckListReno));
+			// Prevent Prince Malchezaar from shuffling a repeat legendary into your deck
+			context.setDeckFormat(new FixedCardsDeckFormat("minion_legendary_test"));
+			context.init();
+			Player player = context.getActivePlayer();
+			context.startTurn(player.getId());
+			// Put Reno into the player's hand
+			Card reno = Stream.concat(player.getDeck().stream(), player.getHand().stream()).filter(c -> c.getCardId().equals("minion_reno_jackson"))
+					.findFirst()
+					.orElseThrow(AssertionError::new);
+			if (reno.getZone() != Zones.HAND) {
+				context.getLogic().receiveCard(player.getId(), reno);
+			}
+			player.getHero().setHp(1);
+			assertEquals(player.getDeck().stream().map(Card::getCardId).distinct().count(), (long) player.getDeck().size());
+			playCard(context, player, reno);
+			assertEquals(player.getHero().getHp(), 30);
+		}
 	}
 
 	@Test
