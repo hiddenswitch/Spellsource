482dabf0d663ed914197d92ace3928ef038b7a02
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-12-12 16:39:22 -0800

    Tweak python deployment script

--- deploy_python.sh
+++ deploy_python.sh
@@ -5,6 +5,7 @@ rm -rf dist/
 mkdir -pv dist
 pip3 install wheel twine >/dev/null
 echo Building
+gradle net:shadowJar
 python3 setup.py sdist bdist_wheel >/dev/null
 echo Deploying
 twine upload dist/*
