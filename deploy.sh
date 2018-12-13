167874d21076cb54d1aa0c5cdaa1f3c4fcf3abf7
Benjamin Berman <benjamin.s.berman@gmail.com>
2018-12-12 19:40:41 -0800

    Bump version

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ cd ..
 zip artifact.zip \
     ./Dockerfile \
     ./Dockerrun.aws.json \
-    ./net/build/libs/net-0.7.9-all.jar \
+    ./net/build/libs/net-0.7.10-all.jar \
     ./server.sh >/dev/null
 
 eb use metastone-dev >/dev/null
