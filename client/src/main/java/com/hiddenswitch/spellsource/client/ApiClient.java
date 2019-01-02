ebf3d41511d218f3aa236f8bc407bdc1b28d6c66
Benjamin Berman <benjamin.s.berman@gmail.com>
2019-01-02 12:44:51 -0800

    At least add the Maven publishing info

--- client/src/main/java/com/hiddenswitch/spellsource/client/ApiClient.java
+++ client/src/main/java/com/hiddenswitch/spellsource/client/ApiClient.java
@@ -84,7 +84,7 @@ public class ApiClient {
     dateFormat = ApiClient.buildDefaultDateFormat();
 
     // Set default User-Agent.
-    setUserAgent("Swagger-Codegen/1.0.0/java");
+    setUserAgent("Swagger-Codegen/0.8.7/java");
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<String, Authentication>();
