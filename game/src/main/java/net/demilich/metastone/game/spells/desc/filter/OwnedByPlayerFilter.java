e0b67dcc6ebadbd98f69362819706731ca419a16
Benjamin Berman <benjamin.s.berman@gmail.com>
2019-01-08 13:50:03 -0800

    Fix OwnedByPlayerFilter

--- game/src/main/java/net/demilich/metastone/game/spells/desc/filter/OwnedByPlayerFilter.java
+++ game/src/main/java/net/demilich/metastone/game/spells/desc/filter/OwnedByPlayerFilter.java
@@ -13,30 +13,6 @@ public class OwnedByPlayerFilter extends EntityFilter {
 
 	@Override
 	protected boolean test(GameContext context, Player player, Entity entity, Entity host) {
-		TargetPlayer targetPlayer = (TargetPlayer) getDesc().get(EntityFilterArg.TARGET_PLAYER);
-
-		switch (targetPlayer) {
-			case ACTIVE:
-				return context.getActivePlayerId() == player.getId();
-			case INACTIVE:
-				return context.getActivePlayerId() != player.getId();
-			case BOTH:
-				return true;
-			case OPPONENT:
-				return entity.getOwner() != player.getId();
-			case SELF:
-				return entity.getOwner() == player.getId();
-			case OWNER:
-				return entity.getOwner() == host.getOwner();
-			case PLAYER_1:
-				return entity.getOwner() == GameContext.PLAYER_1;
-			case PLAYER_2:
-				return entity.getOwner() == GameContext.PLAYER_2;
-			default:
-				break;
-
-		}
-
-		return false;
+		return entity.getOwner() == player.getId();
 	}
 }
