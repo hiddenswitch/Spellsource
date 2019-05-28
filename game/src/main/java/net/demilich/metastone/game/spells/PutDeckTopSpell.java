842433c023f8200090bfe9c0b25cf57b4d1d44e9
doctorpangloss <benjamin.s.berman@gmail.com>
2019-05-27 21:34:35 -0700

    Remaining Spellsource legendaries

--- game/src/main/java/net/demilich/metastone/game/spells/PutDeckTopSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/PutDeckTopSpell.java
@@ -13,6 +13,9 @@ import net.demilich.metastone.game.spells.desc.SpellDesc;
  * Puts the specified cards ({@code target} or otherwise from {@link SpellUtils#getCards(GameContext, Player, Entity,
  * Entity, SpellDesc, int)}) on the top of the deck.
  * <p>
+ * If {@link SpellArg#SECONDARY_VALUE} value is specified, places the card that value many away from the top card. This
+ * implies the default value is {@code 0}.
+ * <p>
  * Puts {@link net.demilich.metastone.game.spells.desc.SpellArg#HOW_MANY} copies.
  *
  * @see ShuffleToDeckSpell to shuffle a card into the deck.
@@ -25,9 +28,10 @@ public final class PutDeckTopSpell extends Spell {
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
 		CardList cards = SpellUtils.getCards(context, player, target, source, desc);
 		int copies = desc.getValue(SpellArg.HOW_MANY, context, player, target, source, 1);
+		int fromTop = desc.getValue(SpellArg.SECONDARY_VALUE, context, player, target, source, 0);
 		for (Card card : cards) {
 			for (int i = 0; i < copies; i++) {
-				if (context.getLogic().insertIntoDeck(player, card.getCopy(), player.getDeck().size())) {
+				if (context.getLogic().insertIntoDeck(player, card.getCopy(), Math.max(0, player.getDeck().size() - fromTop))) {
 					for (SpellDesc subSpell : desc.subSpells(0)) {
 						SpellUtils.castChildSpell(context, player, subSpell, source, target, card);
 					}
