054a8854ebcbe3fe79c3b5b5425ecb7fb4238b35
doctorpangloss <benjamin.s.berman@gmail.com>
2019-07-05 16:59:29 -0700

    Tweak ManaCrystalToMinionSpell

--- game/src/main/java/net/demilich/metastone/game/spells/custom/ManaCrystalToMinionSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/custom/ManaCrystalToMinionSpell.java
@@ -3,6 +3,8 @@ package net.demilich.metastone.game.spells.custom;
 import co.paralleluniverse.fibers.Suspendable;
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
+import net.demilich.metastone.game.cards.Card;
+import net.demilich.metastone.game.cards.CardType;
 import net.demilich.metastone.game.entities.Entity;
 import net.demilich.metastone.game.logic.GameLogic;
 import net.demilich.metastone.game.spells.SpellUtils;
@@ -17,17 +19,22 @@ public class ManaCrystalToMinionSpell extends SummonSpell {
 	@Override
 	@Suspendable
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
-		int cardCost = source.getSourceCard().getBaseManaCost();
 		int maxMana = player.getMaxMana();
 		int summonCount = 0;
 		// Summon minions per summoncount
 		while (player.getMinions().size() < GameLogic.MAX_MINIONS && maxMana > 0) {
-			SpellDesc summonSpell = new SpellDesc(SummonSpell.class);
-			String cardId = (SpellUtils.getCard(context, desc)).getCardId();
-			summonSpell.put(SpellArg.CARD, cardId);
-			super.onCast(context, player, summonSpell, source, target);
-			summonCount++;
-			maxMana--;
+			Card card = SpellUtils.getCard(context, desc);
+			if (card.getCardType() != CardType.MINION) {
+				throw new UnsupportedOperationException("not a minion");
+			}
+
+			boolean summoned = context.getLogic().summon(player.getId(), card.summon(), source, -1, false);
+			if (summoned) {
+				summonCount++;
+				maxMana--;
+			} else {
+				break;
+			}
 		}
 		player.setMaxMana(player.getMaxMana() - summonCount);
 	}
