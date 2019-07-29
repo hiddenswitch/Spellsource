8c9d3e9deea91749407850b6ce530d2b1c802282
Sam Markowitz <wmarkowitz@college.harvard.edu>
2019-07-29 14:03:00 -0700

    Gives Translife Mirror Decay and makes the weapon affected by Gravekeeper Grallows more obviously the one

--- game/src/main/java/net/demilich/metastone/game/spells/AddDescriptionSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/AddDescriptionSpell.java
@@ -6,6 +6,7 @@ import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.cards.Attribute;
 import net.demilich.metastone.game.cards.dynamicdescription.DynamicDescriptionDesc;
 import net.demilich.metastone.game.entities.Entity;
+import net.demilich.metastone.game.spells.desc.SpellArg;
 import net.demilich.metastone.game.spells.desc.SpellDesc;
 
 import java.util.Arrays;
@@ -22,17 +23,24 @@ public class AddDescriptionSpell extends SetDescriptionSpell {
 	@Override
 	@Suspendable
 	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
-		Entity secondaryTarget = context.resolveSingleTarget(player, source, desc.getSecondaryTarget());
-		String description = secondaryTarget.getDescription();
-
-		target.setAttribute(Attribute.DESCRIPTION, target.getDescription() + " " + description);
-
-		DynamicDescriptionDesc[] dynamicDescription = secondaryTarget.getSourceCard().getDynamicDescription();
-		if (dynamicDescription != null && dynamicDescription.length > 0) {
-			target.setAttribute(Attribute.DYNAMIC_DESCRIPTION,
-					Stream.concat(
-							Arrays.stream((DynamicDescriptionDesc[]) target.getAttributes().getOrDefault(Attribute.DYNAMIC_DESCRIPTION,
-									new DynamicDescriptionDesc[0])), Arrays.stream(dynamicDescription)).toArray(DynamicDescriptionDesc[]::new));
+		String description = "";
+		if (desc.containsKey(SpellArg.SECONDARY_TARGET)) {
+			Entity secondaryTarget = context.resolveSingleTarget(player, source, desc.getSecondaryTarget());
+			description = secondaryTarget.getDescription();
+			DynamicDescriptionDesc[] dynamicDescription = secondaryTarget.getSourceCard().getDynamicDescription();
+			if (dynamicDescription != null && dynamicDescription.length > 0) {
+				target.setAttribute(Attribute.DYNAMIC_DESCRIPTION,
+						Stream.concat(
+								Arrays.stream((DynamicDescriptionDesc[]) target.getAttributes().getOrDefault(Attribute.DYNAMIC_DESCRIPTION,
+										new DynamicDescriptionDesc[0])), Arrays.stream(dynamicDescription)).toArray(DynamicDescriptionDesc[]::new));
+			}
+		} else if (desc.containsKey(SpellArg.DESCRIPTION)) {
+			description = desc.getString(SpellArg.DESCRIPTION);
+		}
+		if (target.getDescription().equals("")) {
+			target.setAttribute(Attribute.DESCRIPTION, description);
+		} else {
+			target.setAttribute(Attribute.DESCRIPTION, target.getDescription() + " " + description);
 		}
 	}
 }
