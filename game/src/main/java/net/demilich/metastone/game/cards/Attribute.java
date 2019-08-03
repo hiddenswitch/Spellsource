e116d4abc1f7a1a8989a189eb768e7aca62a592e
doombubbles <obsidiantoolzg@gmail.com>
2019-08-03 11:35:25 -0700

    Soulseeker fixes

--- game/src/main/java/net/demilich/metastone/game/cards/Attribute.java
+++ game/src/main/java/net/demilich/metastone/game/cards/Attribute.java
@@ -1049,10 +1049,6 @@ public enum Attribute {
 	 */
 	LACKEY,
 	/**
-	 * Indicates a minion is a token to be used for the Soulbind effect
-	 */
-	SOULBIND,
-	/**
 	 * Indicates a minion is an official Treant, considered for Treant-related synergies
 	 */
 	TREANT,
--- game/src/main/java/net/demilich/metastone/game/spells/desc/filter/EntityTypeFilter.java
+++ game/src/main/java/net/demilich/metastone/game/spells/desc/filter/EntityTypeFilter.java
@@ -4,12 +4,20 @@ import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
 import net.demilich.metastone.game.entities.Entity;
 import net.demilich.metastone.game.entities.EntityType;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Returns {@code true} if the {@link EntityType} from {@link Entity#getEntityType()} of the {@code target} is equal to
  * the {@link EntityFilterArg#ENTITY_TYPE} argument.
  */
 public final class EntityTypeFilter extends EntityFilter {
+
+	@NotNull
+	public static EntityTypeFilter create(EntityType entityType) {
+		EntityFilterDesc desc = new EntityFilterDesc(AttributeFilter.class);
+		desc.put(EntityFilterArg.ENTITY_TYPE, entityType);
+		return new EntityTypeFilter(desc);
+	}
 
 	public EntityTypeFilter(EntityFilterDesc desc) {
 		super(desc);
