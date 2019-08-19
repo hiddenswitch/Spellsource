2434811bc3f7e3e99233b582ed0465ee866a6c4c
doctorpangloss <benjamin.s.berman@gmail.com>
2019-08-19 08:45:57 -0700

    Afterburner + Smile

--- net/src/main/java/com/hiddenswitch/spellsource/impl/JsonMessageCodec.java
+++ net/src/main/java/com/hiddenswitch/spellsource/impl/JsonMessageCodec.java
@@ -3,6 +3,7 @@ package com.hiddenswitch.spellsource.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.smile.SmileFactory;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import io.netty.buffer.ByteBufInputStream;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.MessageCodec;
@@ -13,7 +14,12 @@ import java.io.InputStream;
 
 public abstract class JsonMessageCodec<T> implements MessageCodec<T, T> {
 
-	private static ObjectMapper mapper = new ObjectMapper(new SmileFactory());
+	private static ObjectMapper mapper;
+
+	static {
+		mapper = new ObjectMapper(new SmileFactory());
+		mapper.registerModule(new AfterburnerModule());
+	}
 
 	protected abstract Class<? extends T> getMessageClass();
 
@@ -37,7 +43,6 @@ public abstract class JsonMessageCodec<T> implements MessageCodec<T, T> {
 	public T decodeFromWire(int pos, Buffer buffer) {
 		int length = buffer.getInt(pos);
 		int start = pos + 4;
-//		ByteBufInputStream stream = new ByteBufInputStream(buffer.getByteBuf());
 		byte[] bytes = buffer.getBytes(start, start + length);
 		try {
 			return mapper.readValue(bytes, getMessageClass());
