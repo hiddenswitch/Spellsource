639368991c6fb44574139ad62358bf24677f91c7
doombubbles <obsidiantoolzg@gmail.com>
2020-01-15 16:25:37 -0800

    Server-side testing of #1440

--- cards/src/test/java/com/hiddenswitch/spellsource/tests/cards/ExileTests.java
+++ cards/src/test/java/com/hiddenswitch/spellsource/tests/cards/ExileTests.java
@@ -1,14 +1,14 @@
 package com.hiddenswitch.spellsource.tests.cards;
 
 import net.demilich.metastone.game.cards.Attribute;
+import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.cards.CardCatalogue;
 import net.demilich.metastone.game.entities.minions.Minion;
 import org.junit.jupiter.api.Test;
 
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ExileTests extends TestBase {
@@ -40,6 +40,34 @@ public class ExileTests extends TestBase {
 			Minion shouldHaveBlitz = playMinionCard(context, player, "minion_line_breaker");
 			assertFalse(shouldHaveBlitz.hasAttribute(Attribute.CHARGE));
 			assertTrue(context.getValidActions().stream().noneMatch(ga -> Objects.equals(ga.getSourceReference(), shouldHaveBlitz.getReference()) && Objects.equals(ga.getTargetReference(), opponent.getHero().getReference())));
+		});
+	}
+
+	@Test
+	public void testObsigonBountySorcerer() {
+		runGym((context, player, opponent) -> {
+			Card obsigon = receiveCard(context, player, "minion_obsigon_bounty_sorcerer");
+			assertEquals(obsigon.getDescription(context, player),
+					"Dash. Opener: Deal 2 damage to a random enemy. Repeat 0 times. (Increases each character that attacked last turn)");
+			Minion attacker = playMinionCard(context, player, "minion_charge_test");
+			attack(context, player, attacker, opponent.getHero());
+			assertEquals(obsigon.getDescription(context, player),
+					"Dash. Opener: Deal 2 damage to a random enemy. Repeat 0 times. (Increases each character that attacked last turn)");
+			context.endTurn();
+			context.endTurn();
+			assertEquals(obsigon.getDescription(context, player),
+					"Dash. Opener: Deal 2 damage to a random enemy. Repeat 1 time. (Increases each character that attacked last turn)");
+		});
+		runGym((context, player, opponent) -> {
+			Card obsigon = receiveCard(context, player, "minion_obsigon_bounty_sorcerer");
+			assertEquals(obsigon.getDescription(context, player),
+					"Dash. Opener: Deal 2 damage to a random enemy. Repeat 0 times. (Increases each character that attacked last turn)");
+			context.endTurn();
+			Minion attacker = playMinionCard(context, opponent, "minion_charge_test");
+			attack(context, player, attacker, player.getHero());
+			context.endTurn();
+			assertEquals(obsigon.getDescription(context, player),
+					"Dash. Opener: Deal 2 damage to a random enemy. Repeat 1 time. (Increases each character that attacked last turn)");
 		});
 	}
 }
