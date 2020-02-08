18fc57750064e6cca56e365789b9c225d591b3ca
doctorpangloss <benjamin.s.berman@gmail.com>
2020-02-07 17:15:06 -0800

    - In progress improvements to libspellsource

--- core/src/main/java/com/hiddenswitch/spellsource/core/ResourceInputStream.java
+++ core/src/main/java/com/hiddenswitch/spellsource/core/ResourceInputStream.java
@@ -3,19 +3,16 @@ package com.hiddenswitch.spellsource.core;
 import java.io.InputStream;
 
 /**
- * Data object that holds a filename, inputstream and a boolean flag to indicate if the file is on the filesystem or in
- * the applications resource bundled dir.
+ * Data object that holds a filename and an input stream.
  */
 public class ResourceInputStream implements CardResource {
 
 	private final String fileName;
 	private final InputStream inputStream;
-	private final boolean fromFilesystem;
 
-	public ResourceInputStream(String fileName, InputStream inputStream, boolean filesystem) {
+	public ResourceInputStream(String fileName, InputStream inputStream) {
 		this.fileName = fileName;
 		this.inputStream = inputStream;
-		this.fromFilesystem = filesystem;
 	}
 
 	@Override
