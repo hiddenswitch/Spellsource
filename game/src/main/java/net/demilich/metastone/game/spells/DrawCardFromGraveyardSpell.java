21b0777c1b8561f9ca4e2b17aa8d1c25c1974a2f
doombubbles <obsidiantoolzg@gmail.com>
2020-04-08 18:32:55 -0700

    Fix #1690

--- game/src/main/java/net/demilich/metastone/game/spells/DrawCardFromGraveyardSpell.java
+++ game/src/main/java/net/demilich/metastone/game/spells/DrawCardFromGraveyardSpell.java
@@ -23,37 +23,42 @@ import java.util.Objects;
  * Draws a card from the graveyard. Alters history!
  */
 public class DrawCardFromGraveyardSpell extends Spell {
-	private static Logger LOGGER = LoggerFactory.getLogger(DrawCardFromGraveyardSpell.class);
+    private static Logger LOGGER = LoggerFactory.getLogger(DrawCardFromGraveyardSpell.class);
 
-	@Override
-	@Suspendable
-	protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
-		checkArguments(LOGGER, context, source, desc, SpellArg.VALUE, SpellArg.CARD_SOURCE, SpellArg.CARD_FILTER);
-		int cardCount = desc.getValue(SpellArg.VALUE, context, player, target, source, 1);
-		CardSourceDesc desc1 = new CardSourceDesc();
-		desc1.put(CardSourceArg.CLASS, GraveyardCardAndActorSourceCardSource.class);
-		desc1.put(CardSourceArg.DISTINCT, true);
-		CardSource cardSource = desc1.create();
-		CardList cards = cardSource.getCards(context, source, player);
-		if (desc.getCardFilter() != null) {
-			cards = cards.filtered(desc.getCardFilter().matcher(context, player, source));
+    @Override
+    @Suspendable
+    protected void onCast(GameContext context, Player player, SpellDesc desc, Entity source, Entity target) {
+        checkArguments(LOGGER, context, source, desc, SpellArg.VALUE, SpellArg.CARD_SOURCE, SpellArg.CARD_FILTER);
+        int cardCount = desc.getValue(SpellArg.VALUE, context, player, target, source, 1);
+		CardSource cardSource;
+        if (!desc.containsKey(SpellArg.CARD_SOURCE)) {
+			CardSourceDesc desc1 = new CardSourceDesc();
+            desc1.put(CardSourceArg.CLASS, GraveyardCardAndActorSourceCardSource.class);
+            desc1.put(CardSourceArg.DISTINCT, true);
+			cardSource = desc1.create();
+        } else {
+        	cardSource = desc.getCardSource();
 		}
-		for (int i = 0; i < cardCount; i++) {
-			if (cards.isEmpty()) {
-				return;
-			}
-			Card card = context.getLogic().removeRandom(cards);
-			Zones originalZone = card.getZone();
-			boolean drawn = context.getLogic().drawCard(player.getId(), card, source) != null;
-			if (drawn) {
-				// Successfully drawn. This minion has now no longer died on the battlefield
-				if (originalZone == Zones.GRAVEYARD) {
-					player.getGraveyard().stream().filter(e -> Objects.equals(e.getSourceCard(), card))
-							.findFirst()
-							.ifPresent(entity -> entity.getAttributes().remove(Attribute.DIED_ON_TURN));
-				}
-			}
-		}
-	}
+        CardList cards = cardSource.getCards(context, source, player);
+        if (desc.getCardFilter() != null) {
+            cards = cards.filtered(desc.getCardFilter().matcher(context, player, source));
+        }
+        for (int i = 0; i < cardCount; i++) {
+            if (cards.isEmpty()) {
+                return;
+            }
+            Card card = context.getLogic().removeRandom(cards);
+            Zones originalZone = card.getZone();
+            boolean drawn = context.getLogic().drawCard(player.getId(), card, source) != null;
+            if (drawn) {
+                // Successfully drawn. This minion has now no longer died on the battlefield
+                if (originalZone == Zones.GRAVEYARD) {
+                    player.getGraveyard().stream().filter(e -> Objects.equals(e.getSourceCard(), card))
+                            .findFirst()
+                            .ifPresent(entity -> entity.getAttributes().remove(Attribute.DIED_ON_TURN));
+                }
+            }
+        }
+    }
 }
 
