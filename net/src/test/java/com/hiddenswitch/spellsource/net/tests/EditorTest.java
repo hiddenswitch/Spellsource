c79099a46eac2710df795dd504afd3b0326e1491
doctorpangloss <benjamin.s.berman@gmail.com>
2020-04-10 20:34:18 -0700

    Make EditorTest less flakey

--- net/src/test/java/com/hiddenswitch/spellsource/net/tests/EditorTest.java
+++ net/src/test/java/com/hiddenswitch/spellsource/net/tests/EditorTest.java
@@ -1,5 +1,6 @@
 package com.hiddenswitch.spellsource.net.tests;
 
+import co.paralleluniverse.strands.Strand;
 import co.paralleluniverse.strands.SuspendableAction2;
 import com.hiddenswitch.spellsource.client.models.Envelope;
 import com.hiddenswitch.spellsource.client.models.EnvelopeMethodPutCard;
@@ -14,22 +15,16 @@ import net.demilich.metastone.game.cards.CardCatalogue;
 import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class EditorTest extends SpellsourceTestBase {
 
 	private void drawCardHarness(TestContext testContext, String code, SuspendableAction2<ServerGameContext, Envelope> handler) {
 		sync(() -> {
-			var keepPlaying = new AtomicBoolean(true);
-			try (var client = new UnityClient(testContext) {
-				@Override
-				protected boolean onRequestAction(ServerToClientMessage message) {
-					return keepPlaying.get();
-				}
-			}) {
+			try (var client = new UnityClient(testContext)) {
 				client.createUserAccount();
 				client.matchmakeQuickPlay(null);
 				client.play(1);
-				keepPlaying.set(false);
 				// Check that the server game context gets edited properly
 				var serverGameContext = getServerGameContext(client.getUserId()).orElseThrow();
 				var envelope = Editor.putCard(new EnvelopeMethodPutCard()
@@ -39,7 +34,6 @@ public class EditorTest extends SpellsourceTestBase {
 						client.getUserId().toString());
 
 				handler.call(serverGameContext, envelope);
-				keepPlaying.set(true);
 				client.respondRandomAction();
 				client.waitUntilDone();
 			}
@@ -50,8 +44,10 @@ public class EditorTest extends SpellsourceTestBase {
 	public void testBasicEditing(TestContext testContext) {
 		var code = Json.encode(CardCatalogue.getCardById(CardCatalogue.getOneOneNeutralMinionCardId()).getDesc());
 		drawCardHarness(testContext, code, (serverGameContext, envelope) -> {
+			var cardId = envelope.getResult().getPutCard().getCardId();
+			testContext.assertNotNull(cardId);
 			testContext.assertNotNull(envelope.getResult().getPutCard().getEditableCardId());
-			testContext.assertTrue(serverGameContext.getPlayers().stream().anyMatch(p -> p.getHand().containsCard(CardCatalogue.getOneOneNeutralMinionCardId())), "the player should have drawn the card");
+			testContext.assertTrue(serverGameContext.getPlayers().stream().anyMatch(p -> p.getHand().containsCard(cardId)), "the player should have drawn the card");
 			testContext.assertTrue(serverGameContext.isRunning());
 		});
 	}
