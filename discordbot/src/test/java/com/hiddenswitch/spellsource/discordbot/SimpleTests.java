eae3992134052b84f9af1aa617ac5bb13d8fba82
doombubbles <obsidiantoolzg@gmail.com>
2020-05-06 15:21:14 -0700

    Add simple non-exact matching to bot

--- discordbot/src/test/java/com/hiddenswitch/spellsource/discordbot/SimpleTests.java
+++ discordbot/src/test/java/com/hiddenswitch/spellsource/discordbot/SimpleTests.java
@@ -10,34 +10,44 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleTests {
 
-		@BeforeAll
-		public static void init() {
-			CardCatalogue.loadAllCards();
-		}
+	@BeforeAll
+	public static void init() {
+		CardCatalogue.loadAllCards();
+	}
 
-		@Test
-		public void testHelpCommand() {
-			Message response = DiscordBot.handleMessage("!help", null);
+	@Test
+	public void testHelpCommand() {
+		Message response = DiscordBot.handleMessage("!help", null);
 
-			assertNotNull(response);
-			assertNotEquals("", response.getContentRaw());
-		}
+		assertNotNull(response);
+		assertNotEquals("", response.getContentRaw());
+	}
 
 	@Test
 	public void testSingleCard() {
 		Message response = DiscordBot.handleMessage("!card soar", null);
 
 		assertNotNull(response);
+		assertTrue(response.getContentRaw().contains(DiscordBot.stringify(CardCatalogue.getCardById("spell_soar"))));
 	}
 
-		@Test
-		public void testMultipleCards() {
-			Message response = DiscordBot.handleMessage("!card devour", null);
+	@Test
+	public void testMultipleCards() {
+		Message response = DiscordBot.handleMessage("!card devour", null);
 
-			assertNotNull(response);
-			assertTrue(response.getContentRaw().contains(DiscordBot.stringify(CardCatalogue.getCardById("spell_devour"))));
-			assertTrue(response.getContentRaw().contains(DiscordBot.stringify(CardCatalogue.getCardById("spell_dominus_discard"))));
-			assertTrue(response.getContentRaw().contains(DiscordBot.stringify(CardCatalogue.getCardById("hero_power_devour"))));
-		}
+		assertNotNull(response);
+		assertTrue(response.getContentRaw().contains(DiscordBot.stringify(CardCatalogue.getCardById("spell_devour"))));
+		assertTrue(response.getContentRaw().contains(DiscordBot.stringify(CardCatalogue.getCardById("spell_dominus_discard"))));
+		assertTrue(response.getContentRaw().contains(DiscordBot.stringify(CardCatalogue.getCardById("hero_power_devour"))));
+	}
+
+	@Test
+	public void testNonExactCards() {
+		Message response = DiscordBot.handleMessage("!card paven", null);
+
+		assertNotNull(response);
+		assertTrue(response.getContentRaw().contains(DiscordBot.stringify(CardCatalogue.getCardById("minion_paven_elemental_of_surprise"))));
+
+	}
 
 }
