ceca44f13fa47f696cc0e5ff6b8578cecff77f6e
doctorpangloss <benjamin.s.berman@gmail.com>
2020-05-06 14:12:22 -0700

    Website hotfixes

--- www/gatsby-node.js
+++ www/gatsby-node.js
@@ -1,5 +1,24 @@
 const path = require(`path`)
 
+exports.onCreateWebpackConfig = ({
+  stage,
+  loaders,
+  actions,
+}) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
+
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
   const typeDefs = `
--- www/src/pages/card-editor.js
+++ www/src/pages/card-editor.js
@@ -1,158 +1,34 @@
-import React, { useState } from 'react'
-import WorkspaceUtils from '../lib/workspace-utils'
+import React from 'react'
+import Loadable from 'react-loadable'
 import Layout from '../components/creative-layout'
-import { useStaticQuery, graphql } from 'gatsby'
-import styles from './card-editor.module.css'
-import ReactBlocklyComponent from 'react-blockly'
-import Blockly from 'blockly'
-import { has, filter } from 'lodash'
-import recursiveOmitBy from 'recursive-omit-by'
-import AceEditor from 'react-ace'
-import 'ace-builds/src-noconflict/mode-json'
-import 'ace-builds/src-noconflict/mode-xml'
-import 'ace-builds/src-noconflict/theme-github'
-import { Xml, FieldLabelSerializable } from 'blockly'
 
-class FieldLabelSerializableHidden extends FieldLabelSerializable {
-  constructor (opt_value, opt_validator, opt_config) {super(opt_value, opt_validator, opt_config)}
+const LoadableComponent = Loadable.Map({
+  loader: {
+    Blockly: () => import('blockly'),
+    CardEditorView: () => import('../components/card-editor-view')
+  },
+  delay: 300,
+  loading () {return (<span>Loading</span>)},
+  render (loaded) {
+    const Blockly = loaded.Blockly
+    const CardEditorView = loaded.CardEditorView.default
 
-  static fromJson (options) {
-    const field = new FieldLabelSerializableHidden()
-    field.setValue(options.value)
-    return field
+    setTimeout(() => {
+      const all = Blockly.Workspace.getAll()
+      for (let i = 0; i < all.length; i++) {
+        const workspace = all[i]
+        if (!workspace.parentWorkspace) {
+          Blockly.svgResize(workspace)
+        }
+      }
+    }, 1)
+    return <CardEditorView/>
   }
-
-  getDisplayText_ () {
-    return ''
-  }
-}
-
-Blockly.fieldRegistry.register('field_label_serializable_hidden', FieldLabelSerializableHidden)
+})
 
 const CardEditor = () => {
-  const [code, setCode] = useState(``)
-  const [xml, setXml] = useState(``)
-  const data = useStaticQuery(graphql`
-  query {
-    toolbox {
-      BlockCategoryList {
-        BlockTypePrefix
-        CategoryName
-        ColorHex
-      }
-    }
-    allBlock {
-      edges {
-        node {
-          id
-          args {
-            i
-            args {
-              type
-              check
-              name
-              valueS
-              valueI
-              min
-              max
-              int
-              text
-            }
-          }
-          colour
-          messages
-          nextStatement
-          output
-          previousStatement
-          type
-          data
-        }
-      }
-    }
-  }`)
-
-  const toolboxCategories = data.toolbox.BlockCategoryList.map(({
-    BlockTypePrefix, CategoryName, ColorHex
-  }) => {
-    return {
-      name: CategoryName,
-      blocks: filter(data.allBlock.edges, edge => edge.node.type.startsWith(BlockTypePrefix))
-        .map(edge => {return { type: edge.node.type }})
-    }
-  })
-
-  data.allBlock.edges.forEach(edge => {
-    if (has(Blockly.Blocks, edge.node.type)) {
-      return
-    }
-
-    const block = recursiveOmitBy(edge.node, ({ node }) => node === null)
-
-    // Patch back in values from union type
-    if (!!block.args) {
-      block.args.forEach(args => {
-        args.args.forEach(arg => {
-          if (!!arg.valueI) {
-            arg.value = arg.valueI
-            delete arg.valueI
-          }
-          if (!!arg.valueS) {
-            arg.value = arg.valueS
-            delete arg.valueS
-          }
-        })
-
-        block['args' + args.i.toString()] = args.args
-      })
-      delete block.args
-    }
-
-    if (!!block.messages) {
-      block.messages.forEach((message, i) => {
-        block['message' + i.toString()] = message
-      })
-      delete block.messages
-    }
-
-    Blockly.Blocks[block.type] = {
-      init: function () {
-        this.jsonInit(block)
-        if (!!block.data) {
-          this.data = block.data
-        }
-      }
-    }
-  })
-
-  function onCodeEditorChanged (newValue) {
-
-  }
-
-  function onWorkspaceChanged (workspace) {
-    setXml('<xml>' + Xml.workspaceToDom(workspace).innerHTML + '</xml>')
-    setCode(JSON.stringify(WorkspaceUtils.workspaceToDictionary(workspace), null, 2))
-  }
-
   return <Layout>
-    <div className={styles.codeEditor}>
-      <ReactBlocklyComponent.BlocklyEditor
-        workspaceDidChange={onWorkspaceChanged}
-        wrapperDivClassName={styles.codeEditor}
-        toolboxCategories={toolboxCategories}/>
-    </div>
-
-    <AceEditor
-      width={'100%'}
-      mode="json"
-      theme="github"
-      setOptions={{
-        'wrap': true
-      }}
-      readOnly={true}
-      onChange={onCodeEditorChanged}
-      value={code}
-      editorProps={{ $blockScrolling: true }}
-    />
+    <LoadableComponent/>
   </Layout>
 }
 
