1bacff0290616ed6fa05bb615356db9f11d3355b
doctorpangloss <benjamin.s.berman@gmail.com>
2020-05-07 09:52:51 -0700

    Final tweaks to discord bot

--- discordbot/src/main/java/com/hiddenswitch/spellsource/discordbot/applications/DiscordBot.java
+++ discordbot/src/main/java/com/hiddenswitch/spellsource/discordbot/applications/DiscordBot.java
@@ -21,17 +21,13 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.EnumSet;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static net.dv8tion.jda.api.MessageBuilder.Formatting.ITALICS;
-import static net.dv8tion.jda.api.MessageBuilder.Formatting.STRIKETHROUGH;
+import static net.dv8tion.jda.api.MessageBuilder.Formatting.*;
 
 
 public class DiscordBot extends ListenerAdapter {
@@ -72,10 +68,9 @@ public class DiscordBot extends ListenerAdapter {
 	public static class Match {
 		final Card card;
 		final boolean exactMatch;
-		final static Comparator<Match> MATCHER = ComparatorUtils.chainedComparator(
-				(m1, m2) -> ComparatorUtils.booleanComparator(true).compare(m1.exactMatch, m2.exactMatch),
-				(m1, m2) -> ComparatorUtils.booleanComparator(true).compare(m1.card.isCollectible(), m2.card.isCollectible())
-		);
+		final static Comparator<Match> MATCHER = Comparator
+				.comparing(Match::isExactMatch, ComparatorUtils.booleanComparator(true))
+				.thenComparing(m -> m.getCard().isCollectible(), ComparatorUtils.booleanComparator(true));
 
 		public Match(Card card, boolean exactMatch) {
 			this.card = card;
@@ -88,6 +83,19 @@ public class DiscordBot extends ListenerAdapter {
 
 		public boolean isExactMatch() {
 			return exactMatch;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (!(o instanceof Match)) return false;
+			Match match = (Match) o;
+			return card.getCardId().equals(match.card.getCardId());
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(card);
 		}
 	}
 
@@ -102,13 +110,9 @@ public class DiscordBot extends ListenerAdapter {
 			var cards = search(nameOrId);
 
 			if (cards.isEmpty()) {
-				messageBuilder.append(" Sorry, I couldn't find anything close to a card with name or ID \"").append(nameOrId).append("\"");
+				messageBuilder.append(" Nothing found for \"").append(nameOrId).append("\"");
 			} else {
-				if (cards.get(0).exactMatch) {
-					messageBuilder.append(" Here you go: ");
-				} else {
-					messageBuilder.append(" I couldn't find an exact match, but it could be: ");
-				}
+				messageBuilder.append(" Results: ");
 
 				for (var match : cards) {
 					sayCard(match.card, messageBuilder);
@@ -120,15 +124,14 @@ public class DiscordBot extends ListenerAdapter {
 				}
 
 				if (cards.size() > MAX_RESULTS) {
-					messageBuilder.append("\n(Capped at 5 results)");
+					messageBuilder.append("\n(More results not shown here)");
 				}
 			}
 
 			response = messageBuilder.build();
 		} else if (messageContent.matches(HELP_COMMAND_REGEX)) {
-			messageBuilder.append(" Currently my only command is the `!card` command.\n");
-			messageBuilder.append("Put it at the start of your message followed by the name or id of a card.\n");
-			messageBuilder.append("If it matches a card, I'll give you its information.\n");
+			messageBuilder.append(" The `!card` command:\n");
+			messageBuilder.append("Put it at the start of your message followed by the name or ID of a card.\n");
 			messageBuilder.append("(An ");
 			messageBuilder.append("italicized", ITALICS);
 			messageBuilder.append(" card name means it's uncollectible, and ");
@@ -148,20 +151,22 @@ public class DiscordBot extends ListenerAdapter {
 	public static List<Match> search(String nameOrId) {
 		return Streams.concat(
 				// Exact match
-				Stream.ofNullable(CardCatalogue.getCards().get(nameOrId)),
+				Stream.ofNullable(Optional.ofNullable(CardCatalogue.getCards().get(nameOrId))
+						.map(c -> new Match(c, true))
+						.orElse((Match) null)),
 				CardCatalogue.getCards()
 						.values()
 						.stream()
 						.filter(c -> c.getName().trim().equalsIgnoreCase(nameOrId.toLowerCase()))
-						.limit(1L)).map(c -> new Match(c, true))
-				.findFirst().map(Collections::singletonList).orElse(
-						// Partial Match
-						CardCatalogue.getCards().values().stream()
-								.filter(c -> c.getName().toLowerCase().contains(nameOrId.toLowerCase())).map(c -> new Match(c, false))
-								.distinct()
-								.sorted(Match.MATCHER)
-								.limit(MAX_RESULTS + 1)
-								.collect(Collectors.toList()));
+						.map(c -> new Match(c, true)),
+				// Partial Match
+				CardCatalogue.getCards().values().stream()
+						.filter(c -> c.getName().toLowerCase().contains(nameOrId.toLowerCase()))
+						.map(c -> new Match(c, false))
+						.sorted(Match.MATCHER)
+						.limit(MAX_RESULTS + 1))
+				.distinct()
+				.collect(Collectors.toList());
 	}
 
 	public static void sayCard(Card card, MessageBuilder messageBuilder) {
@@ -171,13 +176,13 @@ public class DiscordBot extends ListenerAdapter {
 		boolean classCollectible = HeroClass.getClassCard(card.getHeroClass()) == null ||
 				HeroClass.getClassCard(card.getHeroClass()).isCollectible();
 		if (!cardCollectible && !classCollectible) {
-			messageBuilder.append(card.getName(), STRIKETHROUGH, ITALICS);
+			messageBuilder.append(card.getName(), BOLD, STRIKETHROUGH, ITALICS);
 		} else if (!cardCollectible) {
-			messageBuilder.append(card.getName(), ITALICS);
+			messageBuilder.append(card.getName(), BOLD, ITALICS);
 		} else if (!classCollectible) {
-			messageBuilder.append(card.getName(), STRIKETHROUGH);
+			messageBuilder.append(card.getName(), BOLD, STRIKETHROUGH);
 		} else {
-			messageBuilder.append(card.getName());
+			messageBuilder.append(card.getName(), BOLD);
 		}
 
 		messageBuilder.append(" - ").append(stringify(card));
