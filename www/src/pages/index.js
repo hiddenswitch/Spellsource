293eb71b2f82c6e03221764020f8f9b2d2335c3d
Vicki Xu <vickixu20@gmail.com>
2020-07-10 16:52:31 -0700

    fixed index links

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -20,27 +20,6 @@ const Index = () => {
           <ParallaxLayer offset={0} speed={1}
                          style={{ backgroundImage: 'linear-gradient(#21215c, #533B63, #341F42)' }}/>
 
-          <ParallaxLayer offset={1} speed={1.1}>
-            <section style={{ width: '700px', maxWidth: '95%', minHeight: '257.63px' }}>
-              <h3 style={{ textAlign: 'center' }}>
-                <Link to="/download">[Play Now]</Link> - <a href="https://discord.gg/HmbESh2">[Discord]</a> - <Link
-                to="/contribute">[Contribute]</Link>
-              </h3>
-              <p></p>
-              <p>Spellsource is a community-driven digital card game <b>where every card is free</b>. Spellsource features
-                thousands of community-authored cards. </p>
-              <p>Learn more about the game <Link to="/wiki/Main_Page">here</Link> and the keywords <Link
-                to="/keywords">here</Link>. </p>
-              <p>Support us by <Link to="/contribute">authoring your own cards</Link> and <a
-                href="mailto:ben@hiddenswitch.com">contact us</a> about your own art.</p>
-              <p>This is alpha-quality software. You can see more on our <a
-                href="https://github.com/hiddenswitch/Spellsource-Server">GitHub</a>. </p>
-            </section>
-            <section style={{ textAlign: 'center', minHeight: '400px', height: 'auto' }}>
-              <img src={playingGif} className={styles.emphasisGif} alt="in-game gif"/>
-            </section>
-          </ParallaxLayer>
-
           <ParallaxLayer offset={0.99} speed={1} style={{
             backgroundImage: `url(${gameEnvironment})`,
             backgroundSize: 'cover',
@@ -82,6 +61,27 @@ const Index = () => {
             style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
             onClick={() => parallax.scrollTo(1.5)}>
             <img src={spellsource} alt="In-Game Screenshot" style={{ width: '700px', maxWidth: '95%' }}/>
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={1} speed={1.1}>
+            <section style={{ width: '700px', maxWidth: '95%', minHeight: '257.63px' }}>
+              <h3 style={{ textAlign: 'center' }}>
+                <Link to="/download">[Play Now]</Link> - <a href="https://discord.gg/HmbESh2">[Discord]</a> - <Link
+                to="/contribute">[Contribute]</Link>
+              </h3>
+              <p></p>
+              <p>Spellsource is a community-driven digital card game <b>where every card is free</b>. Spellsource features
+                thousands of community-authored cards. </p>
+              <p>Learn more about the game <Link to="/wiki/Main_Page">here</Link> and the keywords <Link
+                to="/keywords">here</Link>. </p>
+              <p>Support us by <Link to="/contribute">authoring your own cards</Link> and <a
+                href="mailto:ben@hiddenswitch.com">contact us</a> about your own art.</p>
+              <p>This is alpha-quality software. You can see more on our <a
+                href="https://github.com/hiddenswitch/Spellsource-Server">GitHub</a>. </p>
+            </section>
+            <section style={{ textAlign: 'center', minHeight: '400px', height: 'auto' }}>
+              <img src={playingGif} className={styles.emphasisGif} alt="in-game gif"/>
+            </section>
           </ParallaxLayer>
         </Parallax>
       </section>
