5817c392cc4e13b357456a6f830725cf12e500e3
Vicki Xu <vickixu20@gmail.com>
2020-07-22 23:30:35 -0700

    deleted unnecessary files:

--- www/src/card-images/export-card-layers.py
+++
@@ -1,8 +0,0 @@
-from psd_tools import PSDImage
-
-psd = PSDImage.open('Spellsource-Large-Cards.psb')
-
-for layer in psd:
-    if ("Large Card Rare" in layer.name and "Large Card Rare A" not in layer.name):
-        image = layer.composite()
-        image.save(layer.name + '.png')
