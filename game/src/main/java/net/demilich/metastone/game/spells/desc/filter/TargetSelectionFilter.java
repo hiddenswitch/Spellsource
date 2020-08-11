3159e6c173b210d16b55f4c028f8fca0168e51d8
doombubbles <obsidiantoolzg@gmail.com>
2020-08-11 14:36:09 -0700

    Finish up Witch

--- game/src/main/java/net/demilich/metastone/game/spells/desc/filter/TargetSelectionFilter.java
+++ game/src/main/java/net/demilich/metastone/game/spells/desc/filter/TargetSelectionFilter.java
@@ -2,9 +2,11 @@ package net.demilich.metastone.game.spells.desc.filter;
 
 import net.demilich.metastone.game.GameContext;
 import net.demilich.metastone.game.Player;
-import net.demilich.metastone.game.cards.Card;
 import net.demilich.metastone.game.entities.Entity;
+import net.demilich.metastone.game.targeting.EntityReference;
 import net.demilich.metastone.game.targeting.TargetSelection;
+
+import java.util.List;
 
 public class TargetSelectionFilter extends EntityFilter {
 	public TargetSelectionFilter(EntityFilterDesc desc) {
@@ -14,6 +16,17 @@ public class TargetSelectionFilter extends EntityFilter {
 	@Override
 	protected boolean test(GameContext context, Player player, Entity entity, Entity host) {
 		var card = entity.getSourceCard();
+
+		if (getDesc().containsKey(EntityFilterArg.SECONDARY_TARGET) && card.isSpell()) {
+			Entity target = context.resolveSingleTarget(player, host, (EntityReference) getDesc().get(EntityFilterArg.SECONDARY_TARGET));
+
+			if (card.isSpell() && !card.canBeCastOn(context, player, target)) {
+				return false;
+			}
+			TargetSelection targetSelection = card.getTargetSelection();
+			List<Entity> entities = context.getTargetLogic().getEntities(context, player, targetSelection, true);
+			return entities.stream().anyMatch(e -> e.getId() == target.getId());
+		}
 
 		var targetSelection = (TargetSelection) getDesc().get(EntityFilterArg.TARGET_SELECTION);
 
