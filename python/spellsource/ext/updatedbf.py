288606a95c5c86780b6b174cc4225f4c8b9f322e
Benjamin Berman <benjamin.s.berman@gmail.com>
2020-09-01 11:00:26 -0700

    Fix python instructions

--- python/spellsource/ext/updatedbf.py
+++ python/spellsource/ext/updatedbf.py
@@ -1,11 +1,15 @@
-from hearthstone import cardxml
-from os import path
-from json import dump
+try:
+    from hearthstone import cardxml
+    from os import path
+    from json import dump
 
 
-def write_dbf_json():
-    dbf = cardxml.load_dbf()[0]
-    new_json = {str(id): card.name for id, card in dbf.items() if card.collectible}
-    f = open(path.join(path.dirname(__file__), '..', '..', 'game', 'src', 'main', 'resources', 'dbf.json'), 'w')
-    dump(new_json, f, indent=2)
-    f.close()
+    def write_dbf_json():
+        dbf = cardxml.load_dbf()[0]
+        new_json = {str(id): card.name for id, card in dbf.items() if card.collectible}
+        f = open(path.join(path.dirname(__file__), '..', '..', 'game', 'src', 'main', 'resources', 'dbf.json'), 'w')
+        dump(new_json, f, indent=2)
+        f.close()
+except ImportError:
+    def write_dbf_json():
+        pass
