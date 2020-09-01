96c522ff5a8eda11af0e3b7c65f6172fb6d6069f
Benjamin Berman <benjamin.s.berman@gmail.com>
2020-08-31 19:35:22 -0700

    Add csharp reference code, add addOrReplaceCards and removeCard methods

--- cards/src/test/java/com/hiddenswitch/spellsource/tests/cards/CardCatalogueTest.java
+++ cards/src/test/java/com/hiddenswitch/spellsource/tests/cards/CardCatalogueTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
-import java.nio.file.Path;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class CardCatalogueTest {
@@ -17,5 +19,37 @@ public class CardCatalogueTest {
 
 		Assertions.assertTrue(CardCatalogue.getAll().size() > 1000);
 		Assertions.assertTrue(CardCatalogue.getBaseClasses(DeckFormat.spellsource()).size() > 5);
+	}
+
+	@Test
+	public void testAddOrReplaceCards() throws IOException {
+		var json = "{\n" +
+				"  \"name\": \"Exampler Test\",\n" +
+				"  \"baseManaCost\": 1,\n" +
+				"  \"type\": \"MINION\",\n" +
+				"  \"heroClass\": \"ANY\",\n" +
+				"  \"baseAttack\": 4,\n" +
+				"  \"baseHp\": 4,\n" +
+				"  \"rarity\": \"EPIC\",\n" +
+				"  \"description\": \"Opener: Summon a 5/5 Skeleton for your opponent\",\n" +
+				"  \"battlecry\": {\n" +
+				"    \"targetSelection\": \"NONE\",\n" +
+				"    \"spell\": {\n" +
+				"      \"class\": \"SummonSpell\",\n" +
+				"      \"card\": \"token_skeletal_enforcer\",\n" +
+				"      \"targetPlayer\": \"OPPONENT\"\n" +
+				"    }\n" +
+				"  },\n" +
+				"  \"attributes\": {\n" +
+				"    \"BATTLECRY\": true\n" +
+				"  },\n" +
+				"  \"collectible\": false,\n" +
+				"  \"set\": \"CUSTOM\",\n" +
+				"  \"fileFormatVersion\": 1\n" +
+				"}";
+		assertEquals("minion_exampler_test", CardCatalogue.addOrReplaceCard(json));
+		assertNotNull(CardCatalogue.getCardById("minion_exampler_test"));
+		CardCatalogue.removeCard("minion_exampler_test");
+		assertThrows(NullPointerException.class, () -> CardCatalogue.getCardById("minion_exampler_test"));
 	}
 }
---
+++ framework/src/csharp/HiddenSwitch/Framework/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Framework
+{
+    public class Class1
+    {
+    }
+}
