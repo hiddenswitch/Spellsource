62d90ee847a893ce264cf7f1085bfb5366ef6575
Benjamin Berman <benjamin.s.berman@gmail.com>
2020-11-17 11:37:34 -0800

    Windows development works now

--- framework/src/main/resources/db/ddl.sql
+++ framework/src/main/resources/db/ddl.sql
@@ -3,7 +3,7 @@
 --
 
 -- Dumped from database version 12.3 (Debian 12.3-1.pgdg100+1)
--- Dumped by pg_dump version 12.3 (Debian 12.3-1.pgdg100+1)
+-- Dumped by pg_dump version 13.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
