893acc0ac8378454c6c300f45a1426dcf458d328
Benjamin Berman <benjamin.s.berman@gmail.com>
2020-12-31 13:37:24 -0800

    Disable another test on CI

--- framework/src/testMigrations/java/com/hiddenswitch/framework/migrations/tests/MigrationTests.java
+++ framework/src/testMigrations/java/com/hiddenswitch/framework/migrations/tests/MigrationTests.java
@@ -6,7 +6,6 @@ import com.hiddenswitch.framework.tests.applications.StandaloneApplication;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testcontainers.junit.jupiter.Container;
