b511d55716b3172925a5549317536587c3bae7b5
Miguel Perez <miguelperez@miguels-air.lan>
2022-12-21 15:56:30 -0500

    added footer without styling. Need to add real links and make pages

--- spellsource-web/src/components/footer.js
+++ spellsource-web/src/components/footer.js
@@ -1,7 +1,20 @@
 import React from "react";
+import * as styles from './creative-layout.module.scss'
+import { Link } from 'gatsby'
 
-function Footer() {
-  return <div>Frooter</div>;
+function Footer({pages}) {
+  return <div className={styles.footer}>
+    <strong>2022 HiddenSwitch, Inc</strong>
+    <ul>
+        <li><Link to="/download">About</Link></li>
+        <li><Link to="/download">Developer API</Link></li>
+        <li><Link to="/download">Disacord</Link></li>
+        <li><Link to="/download">Careers</Link></li>
+        {pages}
+        <li><Link to="/download">Contributors</Link></li>
+        <li><Link to="/download">HiddenSwitch</Link></li>
+    </ul>
+  </div>;
 }
 
 export default Footer;
