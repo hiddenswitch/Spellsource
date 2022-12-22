08d00cc1be40183d9b3d951cbb0d7fe57378c33f
Miguel Perez <Miguel.a.perez4@gmail.com>
2022-12-22 13:31:55 -0500

    added mobile navbar

--- spellsource-web/src/components/header.js
+++ spellsource-web/src/components/header.js
@@ -1,8 +1,13 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import * as styles from './creative-layout.module.scss'
 import Search from './search'
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+import  {AiOutlineMenu} from "@react-icons/all-files/ai/AiOutlineMenu"
+import {AiOutlineClose} from "@react-icons/all-files/ai/AiOutlineClose"
+
+
 
 const Header = ({pages}) => {
   const headerDiv = useRef(null)
@@ -22,20 +27,46 @@ const Header = ({pages}) => {
   }, [])
 
   return <header>
-    <div className={styles.menuContainer} ref={headerDiv} onScroll={e => {handleScroll(e)}}>
-      <a key={'headerImage'}>
-        <Link to="/"><StaticImage src={'../assets/icon.png'} alt={'Icon'} style={{ width: 32, height: 32 }}/>
-          <strong>Spellsource</strong>
-        </Link>
+      <div className={styles.navbarContainer} ref={headerDiv} onScroll={e => {handleScroll(e)}}>
+        <a key={'headerImage'}>
+          <Link to="/"><StaticImage src={'../assets/icon.png'} alt={'Icon'} style={{ width: 32, height: 32 }}/>
+            <strong>Spellsource</strong>
+          </Link>
         </a>
-      <ul>       
-        <li key={'javadocs'}><a href="/javadoc">Docs</a></li>
-        {pages}
-        <li key={'download'}><Link to="/download">Play Now</Link></li>
-        {/* <li key={'search'}><Search placeholder={'Search'}/></li> */}
-      </ul>
-    </div>
+        <DesktopNavbar pages={pages}/>
+        <MobileNavbar pages={pages}/>
+      </div>
   </header>
+}
+
+const DesktopNavbar = ({pages}) => {
+  return (
+    <ul className={styles.desktopNavbar}>       
+      <li key={'javadocs'}><a href="/javadoc">Docs</a></li>
+        {pages}
+      <li key={'download'}><Link to="/download">Play Now</Link></li>
+      {/* <li key={'search'}><Search placeholder={'Search'}/></li> */}
+    </ul>
+  )
+}
+
+const MobileNavbar = ({pages}) => {
+  const [open, setOpen] = useState(false);
+
+  return(
+    <div className={styles.mobileNavbar}>
+      {!open ? 
+        <AiOutlineMenu color="#000" size={32} onClick={()=>{setOpen(!open);}} /> : 
+        <div>
+          <AiOutlineClose color="#000" size={32} onClick={()=>{setOpen(!open);}}/>
+          <ul className={styles.mobileUl}>       
+            <li key={'javadocs'}><a href="/javadoc">Docs</a></li>
+              {pages}
+            <li key={'download'}><Link to="/download">Play Now</Link></li>
+          </ul>
+        </div>}
+  </div>
+  )
 }
 
 export default Header
