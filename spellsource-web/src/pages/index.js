c9631a98a63dfd448261b43c176737a1050ff42b
Miguel Perez <Miguel.a.perez4@gmail.com>
2022-12-21 22:54:41 -0500

    added github section

--- spellsource-web/src/pages/index.js
+++ spellsource-web/src/pages/index.js
@@ -3,13 +3,15 @@ import Layout from '../components/creative-layout'
 import HeroIntroSection from '../components/hero-intro-section'
 import HeroLogoSection from '../components/hero-logo-section'
 import HeroPlaySection from '../components/hero-play-section'
+import HeroGithub from '../components/hero-github'
 
 const Index = () => {
   return (
     <Layout>
       <HeroLogoSection />
       <HeroIntroSection />
-      <HeroPlaySection/>
+      <HeroPlaySection />
+      <HeroGithub />
     </Layout>
   )
 }
