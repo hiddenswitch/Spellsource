99647c66fd5f6612cefa034f86f4eb1d07c80899
Miguel Perez <Miguel.a.perez4@gmail.com>
2022-12-21 22:35:11 -0500

    added play section

--- spellsource-web/src/pages/index.js
+++ spellsource-web/src/pages/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Layout from '../components/creative-layout'
 import HeroIntroSection from '../components/hero-intro-section'
 import HeroLogoSection from '../components/hero-logo-section'
+import HeroPlaySection from '../components/hero-play-section'
 
 const Index = () => {
   return (
     <Layout>
       <HeroLogoSection />
       <HeroIntroSection />
+      <HeroPlaySection/>
     </Layout>
   )
 }
