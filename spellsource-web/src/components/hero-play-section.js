89f9051167821ddfe80b2a5acb6ad54f20208d45
Miguel Perez <Miguel.a.perez4@gmail.com>
2022-12-23 18:37:46 -0500

    updated links

--- spellsource-web/src/components/hero-play-section.js
+++ spellsource-web/src/components/hero-play-section.js
@@ -4,11 +4,6 @@ import playNow from '../assets/play-now.gif'
 import cloud from '../assets/transparent-cloud.png'
 
 function HeroPlaySection() {
-  const title = 'Mobile Beta';
-  const iOSBody = 'You can find the iOS beta here, You will be prompted to join our TestFlight Group, which is Apple’s testing service. You can remain anonymous when you join TestFlight.'
-  const androidBody = 'Join the Spellsource Google Group, then follow the instructions in the welcome message to participate in testing. You must be logged into Google Play to join the testing program. Your e-mail will be shared with the developers. '
-  const pcBody = 'Go to our Discord and ask for the Steam Key.'
-
   return (
     <div className={`${styles.heroContainerAlt} ${styles.backgroundGradient2}`}>
       <img src={cloud} style={{ top: '-10%', left: '8%' }} className={`${styles.cloud} ${styles.lcloud}`}/>
@@ -16,31 +11,23 @@ function HeroPlaySection() {
       <img src={cloud} style={{ bottom: '1%', right: '43%' }} className={`${styles.cloud} ${styles.mcloud}`}/>
       <img src={cloud} style={{ bottom: '3%', right: '-5%' }} className={`${styles.cloud} ${styles.scloud}`}/>
       <img src={cloud} style={{ top: '50%', left: '-11%' }} className={`${styles.cloud} ${styles.scloud}`}/>
+
       <div className={styles.heroPlayContainer}>
-        <SectionTitle title={title}/>
-        <BodySectionTitle title='iOS Users'/>
-        <BodySectionContent text={iOSBody}/>
-        <BodySectionTitle title='Android Users'/>
-        <BodySectionContent text={androidBody}/>
-        <BodySectionTitle title='PC Users'/>
-        <BodySectionContent text={pcBody}/>
+      <h1 className={styles.heroSectionTitle}>Mobile Beta</h1>
+      <h3 className={styles.playSectionBody}>iOS Users</h3>
+      <h4 className={styles.playSectionBody}>You can find the iOS Beta <a href="https://testflight.apple.com/join/pkMfO2qa">here</a>. You will be prompted to join our TestFlight group, which is Apple's testing service. You will remain anonymous when you join Testflight this way.</h4>
+      <h3 className={styles.playSectionBody}>Android Users</h3>
+      <h4 className={styles.playSectionBody}>Join the <a href="https://groups.google.com/forum/#!forum/spellsource-alpha-testers">Spellsource Google Group</a>, then follow the instructions in the welcome message to participate in testing.
+        You must be logged into Google Play to join the testing program. Your e-mail will be shared with the developers.
+      </h4>
+      <h3 className={styles.playSectionBody}>PC Users</h3>
+      <h4 className={styles.playSectionBody}>For users on other browsers, go to our <a href="https://discord.gg/HmbESh2">Discord</a> and ask for the Steam key.</h4>
       </div>
       <div className={styles.heroImageContainer}>
         <img src={playNow} className={styles.heroImage}/>
       </div>
     </div>
   )
-}
-
-const SectionTitle = ({title}) => {
-  return <h1 className={styles.heroSectionTitle}>{title}</h1>
-}
-const BodySectionTitle = ({title}) => {
-  return <h3 className={styles.playSectionTitle}>{title}</h3>
-}
-
-const BodySectionContent = ({text}) => {
-  return <h4 className={styles.playSectionBody}>{text}</h4>
 }
 
 export default HeroPlaySection
