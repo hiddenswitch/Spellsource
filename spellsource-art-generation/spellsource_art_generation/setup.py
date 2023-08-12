714c55471d1698ae91a2d6897e6ad50845300c77
Benjamin Berman <benjamin.s.berman@gmail.com>
2023-08-11 18:46:18 -0700

    fix path issues

--- spellsource-art-generation/spellsource_art_generation/setup.py
+++ spellsource-art-generation/spellsource_art_generation/setup.py
@@ -1,4 +1,16 @@
+import typing
+
 from setuptools import setup, find_packages
+import os
+import sys
+
+"""
+Indicates if we're installing an editable (develop) mode package
+"""
+is_editable = '--editable' in sys.argv or '-e' in sys.argv or (
+        'python' in sys.argv and 'setup.py' in sys.argv and 'develop' in sys.argv)
+
+requirements: typing.List[str] = []
 
 # Read the requirements.txt file
 with open('requirements.txt') as f:
@@ -7,7 +19,7 @@ with open('requirements.txt') as f:
 setup(
     name='spellsource_art_generation',
     version='0.10.0',
-    packages=find_packages(),
+    packages=find_packages(exclude="comfyui"),
     install_requires=requirements,
     author='doctorpangloss',
     author_email='',
